5b54d8765ef6b78ed8b1cc676353ee053fa422db
Fx Benard <fxbenard@gmail.com>
2013-04-27 12:56:49 +0200

    [i18]> Removed bad return

--- includes/class.settings.php
+++ includes/class.settings.php
@@ -414,10 +414,8 @@ class cnRegisterSettings
 			'page_hook' => $settings,
 			'tab'       => 'display',
 			'section'   => 'connections_display_single',
-			'title'     => __('', 'connections'),
-			'desc'      => __('Display a single entry using the active template based on entry type.
-				For example, if the entry is an organization it will be displayed using the template
-				that is activated for the "Organization" template type found on the Connections : Templates admin page.', 'connections'),
+			'title'     => __( '', 'connections' ),
+			'desc'      => __( 'Display a single entry using the active template based on entry type. For example, if the entry is an organization it will be displayed using the template that is activated for the "Organization" template type found on the Connections : Templates admin page.', 'connections' ),
 			'help'      => '',
 			'type'      => 'checkbox',
 			'default'   => 0

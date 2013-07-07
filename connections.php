39f9fe23d46f008fa0e8e4d27401c2c9f066f37b
Steven A. Zahm <helpdesk@connections-pro.com>
2013-07-06 20:58:52 -0400

    Move the templating files to the ../includes/template folder.

--- connections.php
+++ connections.php
@@ -307,9 +307,9 @@ if ( ! class_exists( 'connectionsLoad' ) ) {
 			require_once CN_PATH . 'includes/inc.shortcodes.php'; // Required for front end
 
 			//templates
-			require_once CN_PATH . 'includes/class.template-api.php';
-			require_once CN_PATH . 'includes/class.template-parts.php';
-			require_once CN_PATH . 'includes/class.template.php';
+			require_once CN_PATH . 'includes/template/class.template-api.php';
+			require_once CN_PATH . 'includes/template/class.template-parts.php';
+			require_once CN_PATH . 'includes/template/class.template.php';
 
 			// The class that inits the registered query vars, rewites reuls and canonical redirects.
 			require_once CN_PATH . 'includes/class.rewrite.php';

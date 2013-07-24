cf7f416368ad9e1539e2a5fd8d4a5add8cb48e15
Steven A. Zahm <helpdesk@connections-pro.com>
2013-07-23 20:35:40 -0400

    Check to ensure property isset before checking its value.

--- includes/class.seo.php
+++ includes/class.seo.php
@@ -92,7 +92,7 @@ class cnSEO {
 
 		// Only filter the the permalink for the current post/page being viewed otherwise the nex/prev relational links are filtered too, which we don't want.
 		// Same for the links in the nav, do not change those.
-		if ( $post->ID != $ID || ! self::$filterPermalink ) return $link;
+		if ( ( isset( $post->ID ) &&  $post->ID != $ID ) || ! self::$filterPermalink ) return $link;
 
 
 		if ( $wp_rewrite->using_permalinks() ) {

d9d082b8956f0a4bd07953d934b1024d700ecad7
Steven A. Zahm <helpdesk@connections-pro.com>
2013-08-15 22:01:42 -0400

    Fix the edit links on the dashboard templates.

--- templates/dashboard-recent-added/template.php
+++ templates/dashboard-recent-added/template.php
@@ -14,16 +14,18 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 if ( is_admin() ) {
-	if ( !isset( $form ) ) $form = new cnFormObjects();
 
-	$editTokenURL = $form->tokenURL( 'admin.php?page=connections_manage&action=edit&id=' . $entry->getId(), 'entry_edit_' . $entry->getId() );
+	if ( ! isset( $form ) ) $form = new cnFormObjects();
+
+	$editTokenURL = $form->tokenURL( 'admin.php?page=connections_manage&cn-action=edit_entry&id=' . $entry->getId(), 'entry_edit_' . $entry->getId() );
 
 	if ( current_user_can( 'connections_edit_entry' ) ) {
+
 		echo '<span class="cn-entry-name"><a class="row-title" title="Edit ' , $entry->getName() , '" href="' , $editTokenURL , '"> ' , $entry->getName() . '</a></span> <span class="cn-list-date">' , $entry->getDateAdded( 'm/d/Y g:ia' ) , '</span>';
-	}
-	else {
+
+	} else {
+
 		echo '<span class="cn-entry-name">' , $entry->getName() , '</span> <span class="cn-list-date">' , $entry->getDateAdded( 'm/d/Y g:ia' ) , '</span>';
 	}
 
 }
-?>

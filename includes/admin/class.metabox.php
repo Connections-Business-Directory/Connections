d8e3b9a7ffbbf65dd960334ad63688b6a8752164
Steven A. Zahm <shazahm1@hotmail.com>
2013-09-21 10:32:49 -0400

    Create shell class for the new metabox functionality.

---
+++ includes/admin/class.metabox.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Class registering and creating the metabox for add/edit an entry.
+ *
+ * @package     Connections
+ * @subpackage  Metabox
+ * @copyright   Copyright (c) 2013, Steven A. Zahm
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.7.9
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class cnMetabox {
+
+	/**
+	 * Stores the instance of this class.
+	 *
+	 * @access private
+	 * @since 0.7.9
+	 * @var (object)
+	*/
+	private static $instance;
+
+	/**
+	 * A dummy constructor to prevent the class from being loaded more than once.
+	 *
+	 * @access public
+	 * @since 0.7.9
+	 * @see cnMetabox::init()
+	 * @see cnMetabox();
+	 */
+	public function __construct() { /* Do nothing here */ }
+
+	/**
+	 * Setup the class, if it has already been initialized, return the intialized instance.
+	 *
+	 * @access public
+	 * @since 0.7.9
+	 * @see cnMetabox()
+	 */
+	public static function init() {
+
+		if ( ! isset( self::$instance ) ) {
+
+			self::$instance = new self;
+
+
+		}
+
+	}
+
+	/**
+	 * Return an instance of the class.
+	 *
+	 * @access public
+	 * @since 0.7.9
+	 * @return (object) cnMetabox
+	 */
+	public static function getInstance() {
+
+		return self::$instance;
+	}
+
+}

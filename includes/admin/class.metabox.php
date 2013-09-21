2df10f2efa55565892bbce0b6249ae26d70b2431
Steven A. Zahm <shazahm1@hotmail.com>
2013-09-21 10:42:14 -0400

    Correct the @since version number.

--- includes/admin/class.metabox.php
+++ includes/admin/class.metabox.php
@@ -7,7 +7,7 @@
  * @subpackage  Metabox
  * @copyright   Copyright (c) 2013, Steven A. Zahm
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
- * @since       0.7.9
+ * @since       0.8
  */
 
 // Exit if accessed directly
@@ -19,7 +19,7 @@ class cnMetabox {
 	 * Stores the instance of this class.
 	 *
 	 * @access private
-	 * @since 0.7.9
+	 * @since 0.8
 	 * @var (object)
 	*/
 	private static $instance;
@@ -28,7 +28,7 @@ class cnMetabox {
 	 * A dummy constructor to prevent the class from being loaded more than once.
 	 *
 	 * @access public
-	 * @since 0.7.9
+	 * @since 0.8
 	 * @see cnMetabox::init()
 	 * @see cnMetabox();
 	 */
@@ -38,7 +38,7 @@ class cnMetabox {
 	 * Setup the class, if it has already been initialized, return the intialized instance.
 	 *
 	 * @access public
-	 * @since 0.7.9
+	 * @since 0.8
 	 * @see cnMetabox()
 	 */
 	public static function init() {
@@ -56,7 +56,7 @@ class cnMetabox {
 	 * Return an instance of the class.
 	 *
 	 * @access public
-	 * @since 0.7.9
+	 * @since 0.8
 	 * @return (object) cnMetabox
 	 */
 	public static function getInstance() {

6f64c15e34367b37d6938ffaa9a9cb9a08e49f8f
Steven A. Zahm <shazahm1@hotmail.com>
2013-12-02 21:19:10 -0500

    Add plugin header to core template.
    gh-269

--- templates/card/card.php
+++ templates/card/card.php
@@ -117,7 +117,7 @@ if ( ! class_exists( 'CN_Card_Template' ) ) {
 
 	}
 
-	// This action is ran if loaded by core.
+	// This action is ran if the template is loaded by core to register the template.
 	add_action( 'cn_register_template', array( 'CN_Card_Template', 'register' ) );
 
 	// If this template is moved to the `plugins` folder, this action will be run to register the template.

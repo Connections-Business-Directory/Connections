77d6e3f8c5b1dfa2d8b136cee110d1d52459849b
Steven A. Zahm <shazahm1@hotmail.com>
2013-12-14 13:31:45 -0500

    Remove use of $wpdb->escape().
    Closes gh-302

--- includes/class.terms.php
+++ includes/class.terms.php
@@ -363,7 +363,7 @@ class cnTerms
 	 * @return bool
 	 */
 	public function updateTerm( $termID, $taxonomy, $attributes ) {
-		global $wpdb, $connections;
+		global $wpdb;
 
 		$name        = $attributes['name'];
 		$slug        = $attributes['slug'];
@@ -377,40 +377,46 @@ class cnTerms
 		 * Why can't a row be updated that must have a unique value
 		 * if the slug value isn't being changed??????
 		 */
-		$sql = "UPDATE " . CN_TERMS_TABLE . " SET
-				slug		= ''
-				WHERE term_id = '" . $wpdb->escape( $termID ) . "'";
-
-		// If insert fails return NULL.
-		if ( ! $wpdb->query( $sql ) ) return;
-		unset( $sql );
+		$wpdb->update(
+			CN_TERMS_TABLE,
+			array( 'slug' => '' ),
+			array( 'term_id' => $termID ),
+			'%s',
+			'%d'
+			);
 
 		$slug = $this->getUniqueSlug( $slug, $name );
 
-		$sql = "UPDATE " . CN_TERMS_TABLE . " SET
-			name		= '" . $wpdb->escape( $name ) . "',
-			slug		= '" . $wpdb->escape( $slug ) . "',
-			term_group	= '0'
-			WHERE term_id = '" . $wpdb->escape( $termID ) . "'";
-
-		// If insert fails return NULL.
-		if ( ! $wpdb->query( $sql ) ) return;
-		unset( $sql );
+		$wpdb->update(
+			CN_TERMS_TABLE,
+			array(
+				'name'       => $name,
+				'slug'       => $slug,
+				'term_group' => 0
+				),
+			array( 'term_id' => $termID ),
+			array( '%s', '%s', '%d' ),
+			'%d'
+			);
 
 		$ttID = $wpdb->get_var( $wpdb->prepare( "SELECT tt.term_taxonomy_id FROM " . CN_TERM_TAXONOMY_TABLE . " AS tt INNER JOIN " . CN_TERMS_TABLE . " AS t ON tt.term_id = t.term_id WHERE tt.taxonomy = %s AND t.term_id = %d", $taxonomy, $termID ) );
 
-		$sql = "UPDATE " . CN_TERM_TAXONOMY_TABLE . " SET
-			term_id		= '" . $wpdb->escape( $termID ) . "',
-			taxonomy	= '" . $wpdb->escape( $taxonomy ) . "',
-			description	= '" . $wpdb->escape( $description ) . "',
-			parent		= '" . $wpdb->escape( $parent ) . "'
-			WHERE term_taxonomy_id 	= '" . $wpdb->escape( $ttID ) . "'";
+		$wpdb->update(
+			CN_TERM_TAXONOMY_TABLE,
+			array(
+				'term_id'     => $termID,
+				'taxonomy'    => $taxonomy,
+				'description' => $description,
+				'parent'      => $parent
+				),
+			array( 'term_taxonomy_id' => $ttID ),
+			array( '%d', '%s', '%s', '%d' ),
+			'%d'
+			);
 
 		/**
 		 * @TODO: Error check the insert and return error
 		 */
-		$wpdb->query( $sql );
-		unset( $sql );
 
 		return TRUE;
 	}

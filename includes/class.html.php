5d3e9f40f57c5507b00d02d182f43ee1b9fc09b8
Steven A. Zahm <shazahm1@hotmail.com>
2014-01-03 15:34:55 -0500

    Add checkbox and checkbox group to cnHTML.

--- includes/class.html.php
+++ includes/class.html.php
@@ -15,6 +15,177 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 class cnHTML {
 
+	/**
+	 * Helper method that can be used within loops to
+	 * dynamically call the correct field type to render.
+	 *
+	 * @access public
+	 * @since 0.8
+	 * @param  array  $atts  The field attirbutes array.
+	 * @param  string $value The field value.
+	 *
+	 * @return string        The rendered field.
+	 */
+	public static function field( $atts, $value = '' ) {
+
+		switch ( $atts['type'] ) {
+
+			case 'text':
+
+				return self::text( $atts, $value );
+
+				break;
+
+			case 'checkbox':
+
+				return self::checkbox( $atts, $value );
+
+				break;
+
+			case 'checkbox_group':
+
+				return self::checkboxGroup( $atts, $value );
+
+				break;
+
+			case 'radio':
+
+				return self::radio( $atts, $value );
+
+				break;
+
+			case 'select':
+
+				return self::select( $atts, $value );
+
+				break;
+
+			default:
+				# todo Put action and or filter here.
+				break;
+		}
+	}
+
+	/**
+	 * Renders a text input field.
+	 *
+	 * @access public
+	 * @since 0.8
+	 * @param  array  $atts  The field attributes.
+	 * @param  string $value The field value.
+	 *
+	 * @return string        The rendered field.
+	 */
+	public static function text( $atts, $value = '' ) {
+
+		$atts['type'] = 'text';
+
+		return self::input( $atts, $value );
+	}
+
+	/**
+	 * Renders a checkbox field.
+	 *
+	 * @access public
+	 * @since 0.8
+	 * @param  array  $atts  The field attributes.
+	 * @param  string $value The field value.
+	 *
+	 * @return string        The rendered field.
+	 */
+	public static function checkbox( $atts, $value = '' ) {
+
+		$atts['type']    = 'checkbox';
+		$atts['layout']  = '%field%%label%';
+		$atts['checked'] = checked( '1', $value, FALSE );
+
+		return self::input( $atts, '1' );
+	}
+
+	/**
+	 * Renders a group of checkboxes.
+	 *
+	 * @todo This has not been tested att all, will likely contain bugs or not work correctly at all.
+	 *
+	 * @access public
+	 * @since 0.8
+	 * @param  array  $atts  The field attributes.
+	 * @param  string $value The field value.
+	 *
+	 * @return string        The rendered field group.
+	 */
+	public static function checkboxGroup( $atts, $value = '' ) {
+
+		$atts['type'] = 'checkbox';
+
+		return self::group( $atts, $value );
+	}
+
+	/**
+	 * Renders a radio group.
+	 *
+	 * @access public
+	 * @since 0.8
+	 * @param  array  $atts  The field attributes.
+	 * @param  string $value The field value.
+	 *
+	 * @return string        The rendered field group.
+	 */
+	public static function radio( $atts, $value = '' ) {
+
+		$atts['type'] = 'radio';
+
+		return self::group( $atts, $value );
+	}
+
+	/**
+	 * Prefixes the supplied string with the defined prefix.
+	 *
+	 * @access public
+	 * @since 0.8
+	 * @uses wp_parse_args()
+	 * @param  mixed  $value string | array  The value to add the defined prefix to.
+	 * @param  array  $atts  The attrubutes array.
+	 *
+	 * @return mixed         string | array
+	 */
+	public static function prefix( $value, $atts = array() ) {
+
+		if ( empty( $value ) ) return;
+
+		$defaults = array(
+			'prefix'   => 'cn-',
+			);
+
+		$atts = wp_parse_args( $atts, $defaults );
+
+		if ( is_array( $value ) ) {
+
+			foreach ( $value as $key => $class ) {
+
+				$value[ $key ] = $atts['prefix'] . $class;
+			}
+
+			return $value;
+
+		} else {
+
+			return $atts['prefix'] . $value;
+		}
+	}
+
+	/**
+	 * Renders a HTML tag attribute.
+	 *
+	 * @access public
+	 * @since 0.8
+	 * @uses sanitize_html_class()
+	 * @uses esc_attr()
+	 * @param  string $type  The attribute name.
+	 * @param  string $value The attribute value.
+	 *
+	 * @return string        The rendered attribute.
+	 */
 	public static function attribute( $type, $value ) {
 
 		switch ( $type ) {
@@ -92,59 +263,17 @@ class cnHTML {
 
 	}
 
-	public static function field( $atts, $value = '' ) {
-
-		switch ( $atts['type'] ) {
-
-			case 'text':
-
-				return self::input( $atts, $value );
-
-				break;
-
-			case 'radio':
-
-				return self::radio( $atts, $value );
-
-				break;
-
-			case 'select':
-
-				return self::select( $atts, $value );
-
-				break;
-
-			default:
-				# todo Put action and or filter here.
-				break;
-		}
-	}
-
-	private static function prefix( $value, $atts = array() ) {
-
-		if ( empty( $value ) ) return;
-
-		$defaults = array(
-			'prefix'   => 'cn-',
-			);
-
-		$atts = wp_parse_args( $atts, $defaults );
-
-		if ( is_array( $value ) ) {
-
-			foreach ( $value as $key => $class ) {
-
-				$value[ $key ] = $atts['prefix'] . $class;
-			}
-
-			return $value;
-
-		} else {
-
-			return $atts['prefix'] . $value;
-		}
-	}
-
+	/**
+	 * Render a field lsbel.
+	 *
+	 * @access public
+	 * @since 0.8
+	 * @uses wp_parse_args()
+	 * @uses esc_attr()
+	 * @param  array $atts  The label attributes.
+	 *
+	 * @return string       The rendered label.
+	 */
 	public static function label( $atts ) {
 
 		$defaults = array(
@@ -173,14 +302,28 @@ class cnHTML {
 		echo $out;
 	}
 
-	public static function input( $atts, $value = '' ) {
+	/**
+	 * Renders an input field of the supplied type.
+	 *
+	 * @access private
+	 * @since 0.8
+	 * @uses wp_parse_args()
+	 * @param  array  $atts  The field attributes.
+	 * @param  string $value The field value.
+	 *
+	 * @return string        The rendered field.
+	 */
+	private static function input( $atts, $value = '' ) {
 
 		$defaults = array(
+			'type'     => 'text',
 			'prefix'   => 'cn-',
 			'class'    => array(),
 			'id'       => '',
+			'name'     => '',
 			'style'    => array(),
-			'value'    => '',
+			// 'value'    => '',
+			'ckecked'  => '',
 			'required' => FALSE,
 			'label'    => '',
 			'before'   => '',
@@ -195,8 +338,8 @@ class cnHTML {
 		// If no `id` was supplied, bail.
 		if ( empty( $atts['id'] ) ) return '';
 
-		// The field name.
-		$name = $atts['id'];
+		// The field name. If not supplied, use the id.
+		$name = ! empty( $atts['name'] ) ? $atts['name'] : $atts['id'];
 
 		// The field parts to be searched for in $atts['layout'].
 		$search = $atts['parts'];
@@ -219,12 +362,14 @@ class cnHTML {
 		// Create the field label, if supplied.
 		$replace[] = ! empty( $atts['label'] ) ? self::label( array( 'for' => $atts['id'], 'label' => $atts['label'], 'return' => TRUE ) ) : '';
 
-		$replace[] = sprintf( '<input type="text" %1$s %2$s %3$s %4$s %5$s/>',
+		$replace[] = sprintf( '<input %1$s %2$s %3$s %4$s %5$s %6$s %7$s/>',
+			self::attribute( 'type', $atts['type'] ),
 			self::attribute( 'class', $atts['class'] ),
 			self::attribute( 'id', $atts['id'] ),
 			self::attribute( 'name', $name ),
 			self::attribute( 'style', $atts['style'] ),
-			self::attribute( 'value', $value )
+			self::attribute( 'value', $value ),
+			! empty( $atts['checked'] ) ? $atts['checked'] : ''
 		);
 
 		$out = str_ireplace( $search, $replace, $atts['layout'] );
@@ -236,11 +381,25 @@ class cnHTML {
 		echo ( empty( $atts['before'] ) ? '' : $atts['before'] ) . $out . ( empty( $atts['after'] ) ? '' : $atts['after'] );
 	}
 
-	public static function radio( $atts, $value = '' ) {
+	/**
+	 * Renders either a radio or checkbox group.
+	 *
+	 * @access private
+	 * @since 0.8
+	 * @uses wp_parse_args()
+	 * @uses esc_attr()
+	 * @uses checked()
+	 * @param  array  $atts  The group attributes.
+	 * @param  string $value The group item that will be marked as "CHECKED".
+	 *
+	 * @return string        The rendered group.
+	 */
+	private static function group( $atts, $value = '' ) {
 
 		$out = '';
 
 		$defaults = array(
+			'type'     => 'radio',
 			'prefix'   => 'cn-',
 			'class'    => array( 'radio' ),
 			'id'       => '',
@@ -279,25 +438,31 @@ class cnHTML {
 		// and then tack the "required" value to the end of the array.
 		$atts['class'] = $atts['required'] ? array_merge( (array) $atts['class'], array('required') ) : $atts['class'];
 
-		$out .= '<span class="cn-radio-group" style="display: ' . esc_attr( $atts['display'] ) . ';">';
+		$out .= '<span class="cn-' . esc_attr( $atts['type'] ) . '-group" style="display: ' . esc_attr( $atts['display'] ) . ';">';
 
 		foreach ( $atts['options'] as $key => $label ) {
 
 			// An array to store the replacement strings for the label and field.
 			$replace = array();
 
-			$out .= '<span class="cn-radio-option" style="display: ' . esc_attr( $atts['display'] ) . ';">';
+			$out .= '<span class="cn-' . esc_attr( $atts['type'] ) . '-option" style="display: ' . esc_attr( $atts['display'] ) . ';">';
 
 			// Create the field label, if supplied.
 			$replace[] = ! empty( $label ) ? self::label( array( 'for' => $atts['id'] . '[' . $key . ']', 'label' => $label, 'return' => TRUE ) ) : '';
 
-			$replace[] = sprintf( '<input type="radio" %1$s %2$s %3$s %4$s %5$s %6$s/>',
-				self::attribute( 'class', $atts['class'] ),
-				self::attribute( 'id', $atts['id'] . '[' . $key . ']' ),
-				self::attribute( 'name', $name ),
-				self::attribute( 'style', $atts['style'] ),
-				self::attribute( 'value', $value ),
-				checked( TRUE , in_array( $key, (array) $value ) , FALSE )
+			$replace[] = self::input(
+				array(
+					'type'    => $atts['type'],
+					'prefix'  => '',
+					'class'   => $atts['class'],
+					'id'      => $atts['id'] . '[' . $key . ']',
+					'name'    => $name,
+					'style'   => $atts['style'],
+					'value'   => $value,
+					'checked' => checked( TRUE , in_array( $key, (array) $value ) , FALSE ),
+					'return'  => TRUE,
+					),
+				$key
 			);
 
 			$out .= str_ireplace( $search, $replace, $atts['layout'] );
@@ -314,6 +479,20 @@ class cnHTML {
 		echo PHP_EOL . ( empty( $atts['before'] ) ? '' : $atts['before'] ) . $out . ( empty( $atts['after'] ) ? '' : $atts['after'] ) . PHP_EOL;
 	}
 
+	/**
+	 * Renders a select field.
+	 *
+	 * @access public
+	 * @since 0.8
+	 * @uses wp_parse_args()
+	 * @uses esc_attr()
+	 * @uses selected()
+	 * @uses esc_html()
+	 * @param  array  $atts  The field attributes.
+	 * @param  string $value The selected option.
+	 *
+	 * @return string        The rendered field.
+	 */
 	public static function select( $atts, $value = '' ) {
 
 		$out = '';

9d6adf5427e7f7a76ac1b3ffbe5edfdc5b6fe67e
Steven A. Zahm <shazahm1@hotmail.com>
2014-01-30 22:04:42 -0500

    Add shell for cnEntry_HTML.

---
+++ includes/class.entry-html.php
@@ -0,0 +1,19 @@
+<?php
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Class to be used to render entry HTML.
+ * This is a work in progress, when complete it will replace cnOutput.
+ *
+ * @package     Connections
+ * @subpackage  Entry HTML Output
+ * @copyright   Copyright (c) 2013, Steven A. Zahm
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.8
+ */
+
+class cnEntry_HTML extends cnOutput {
+
+}

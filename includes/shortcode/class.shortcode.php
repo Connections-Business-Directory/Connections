0fba2b24ee4619ebc4913724b584e1f42dd1e700
Steven A. Zahm <shazahm1@hotmail.com>
2014-04-23 15:38:36 -0400

    Remove commented out code.

--- includes/shortcode/class.shortcode.php
+++ includes/shortcode/class.shortcode.php
@@ -88,8 +88,6 @@ class cnShortcode {
 
 		// Register the core shortcodes.
 		add_shortcode( 'connections', 'connectionsView' );
-		// add_shortcode( 'connections', array( 'cnShortcode_Connections', 'shortcode' ) ); /* The new [connections] shortcode. */
-		// add_shortcode( 'connections_list', 'connectionsView' ); /* @deprecated since version 0.7.0.4 */
 		add_shortcode( 'upcoming_list', '_upcoming_list' );
 		add_shortcode( 'connections_vcard', '_connections_vcard' ); /* Experimental. Do NOT use. */
 		add_shortcode( 'connections_qtip', '_connections_qtip' ); /* Experimental. Do NOT use. */

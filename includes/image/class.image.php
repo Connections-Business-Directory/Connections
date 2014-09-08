a0fab3b8d6990a6d3d03541c359dd7fcc155538a
Steven A. Zahm <shazahm1@hotmail.com>
2014-09-08 10:51:08 -0400

    Add error message for when $img_path is not set.

--- includes/image/class.image.php
+++ includes/image/class.image.php
@@ -689,7 +689,15 @@ class cnImage {
 		// Fail if we can't find the image in our WP local directory
 		if ( empty( $img_path ) || ! @file_exists( $img_path ) ) {
 
-			return new WP_Error( 'image_path_not_found', __( sprintf( 'Image path %s does not exist.', $img_path ), 'connections' ), $img_path );
+			if ( empty( $img_path) ) {
+
+				return new WP_Error( 'image_path_not_set', esc_html__( 'The $img_path variable has not been set.', 'connections' ) );
+
+			} else {
+
+				return new WP_Error( 'image_path_not_found', __( sprintf( 'Image path %s does not exist.', $img_path ), 'connections' ), $img_path );
+			}
+
 		}
 
 		// Check if img path exists, and is an image.

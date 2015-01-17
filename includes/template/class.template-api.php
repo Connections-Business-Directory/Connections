422113241bf2fc622d34e3d86b09677cef30da83
Steven A. Zahm <shazahm1@hotmail.com>
2015-01-16 21:13:55 -0500

    Run cnTemplateFactory::init() at a lower priority so it runs last on the plugins_loaded action.

--- includes/template/class.template-api.php
+++ includes/template/class.template-api.php
@@ -590,5 +590,6 @@ class cnTemplateFactory {
 }
 
 // Init the Template Factory API
+// NOTE: The priority can not be > 10 otherwise it will break older templates which init'd on `plugins_loaded` at priority 11.
 // cnTemplateFactory::init();
-add_action( 'plugins_loaded', array( 'cnTemplateFactory', 'init' ), 99 );
+add_action( 'plugins_loaded', array( 'cnTemplateFactory', 'init' ) );

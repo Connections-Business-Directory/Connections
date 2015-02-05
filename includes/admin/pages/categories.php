02ef114d6761f0da1a87ecc637c4ea8237e779b8
Steven A. Zahm <shazahm1@hotmail.com>
2015-02-05 16:24:36 -0500

    Completely refactored of the categories admin page.

--- includes/admin/pages/categories.php
+++ includes/admin/pages/categories.php
@@ -11,15 +11,19 @@
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 function connectionsShowCategoriesPage() {
-	/*
-	 * Check whether user can edit catgories.
-	 */
-	if ( !current_user_can( 'connections_edit_categories' ) ) {
 
-		wp_die( '<p id="error-page" style="-moz-background-clip:border;
+	/*
+	 * Check whether user can edit categories.
+	 */
+	if ( ! current_user_can( 'connections_edit_categories' ) ) {
+
+		wp_die(
+			'<p id="error-page" style="-moz-background-clip:border;
 				-moz-border-radius:11px;
 				background:#FFFFFF none repeat scroll 0 0;
 				border:1px solid #DFDFDF;
@@ -30,25 +34,46 @@ function connectionsShowCategoriesPage() {
 				margin:25px auto 20px;
 				padding:1em 2em;
 				text-align:center;
-				width:700px">' . __( 'You do not have sufficient permissions to access this page.', 'connections' ) . '</p>' );
+				width:700px">' . __(
+				'You do not have sufficient permissions to access this page.',
+				'connections'
+			) . '</p>'
+		);
 
 	} else {
 
-		global $connections;
+		// Grab an instance of the Connections object.
+		$instance = Connections_Directory();
 
-		$form = new cnFormObjects();
-		$categoryObjects = new cnCategoryObjects();
+		$form     = new cnFormObjects();
+		$taxonomy = 'category';
+		$action   = '';
 
 		if ( isset( $_GET['cn-action'] ) ) {
+
 			$action = $_GET['cn-action'];
-		} else {
-			$action = NULL;
 		}
 
 		if ( $action === 'edit_category' ) {
 
-			$id = esc_attr( $_GET['id'] );
+			$id = absint( $_GET['id'] );
 			check_admin_referer( 'category_edit_' . $id );
+
+			$term     = $instance->retrieve->category( $id );
+			$category = new cnCategory( $term );
+
+			/**
+			 * Fires before the Edit Term form for all taxonomies.
+			 *
+			 * The dynamic portion of the hook name, `$taxonomy`, refers to
+			 * the taxonomy slug.
+			 *
+			 * @since 3.0.0
+			 *
+			 * @param object $tag      Current taxonomy term object.
+			 * @param string $taxonomy Current $taxonomy slug.
+			 */
+			do_action( "cn_{$taxonomy}_pre_edit_form", $term, $taxonomy );
 
 			?>
 
@@ -67,92 +92,196 @@ function connectionsShowCategoriesPage() {
 					$form->open( $attr );
 					$form->tokenField( 'update_category' );
 
-					$categoryObjects->showForm( $connections->retrieve->category( $id ) );
+					/**
+					 * Fires inside the Edit Term form tag.
+					 *
+					 * The dynamic portion of the hook name, `$taxonomy`, refers to
+					 * the taxonomy slug.
+					 *
+					 * @since 3.7.0
+					 */
+					do_action( "cn_{$taxonomy}_term_edit_form_tag" );
+					?>
+
+					<div class="form-field form-required term-name-wrap">
+						<label for="category_name"><?php _e( 'Name', 'connections' ) ?></label>
+						<input type="text" aria-required="true" size="40" value="<?php echo esc_attr( $category->getName() ); ?>" id="category_name" name="category_name"/>
+						<input type="hidden" value="<?php echo esc_attr( $category->getID() ); ?>" id="category_id" name="category_id"/>
+
+						<p><?php _e( 'The name is how it appears on your site.', 'connections' ); ?></p>
+					</div>
+
+					<div class="form-field term-slug-wrap">
+						<label for="category_slug"><?php _e( 'Slug', 'connections' ); ?> </label>
+						<input type="text" size="40" value="<?php echo esc_attr( $category->getSlug() ); ?>" id="category_slug" name="category_slug"/>
+
+						<p><?php _e(
+								'The “slug” is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.',
+								'connections'
+							); ?></p>
+					</div>
+
+					<div class="form-field term-parent-wrap">
+						<label for="category_parent"><?php _e( 'Parent', 'connections' ); ?></label>
+
+						<?php
+						cnTemplatePart::walker(
+							'term-select',
+							array(
+								'hide_empty'       => 0,
+								'hide_if_empty'    => FALSE,
+								'name'             => 'category_parent',
+								'orderby'          => 'name',
+								'taxonomy'         => 'category',
+								'selected'         => $category->getParent(),
+								'exclude_tree'     => $category->getID(),
+								'hierarchical'     => TRUE,
+								'show_option_none' => __( 'None' ),
+								//'return'           => TRUE,
+							)
+						);
+						?>
+						<p><?php _e(
+								'Categories can have a hierarchy. You might have a Jazz category, and under that have children categories for Bebop and Big Band. Totally optional.',
+								'connections'
+							); ?></p>
+					</div>
+
+
+					<div class="form-field term-description-wrap">
+						<?php
+						ob_start();
+
+						/*
+						 * Now we're going to have to keep track of which TinyMCE plugins
+						 * WP core supports based on version, sigh.
+						 */
+						if ( version_compare( $GLOBALS['wp_version'], '3.8.999', '<' ) ) {
+
+							$tinymcePlugins = array(
+								'inlinepopups',
+								'tabfocus',
+								'paste',
+								'wordpress',
+								'wplink',
+								'wpdialogs'
+							);
+
+						} else {
+
+							$tinymcePlugins = array( 'tabfocus', 'paste', 'wordpress', 'wplink', 'wpdialogs' );
+						}
+
+						wp_editor(
+							wp_kses_post( $category->getDescription() ),
+							'category_description',
+							array(
+								'media_buttons' => FALSE,
+								'tinymce'       => array(
+									'editor_selector'   => 'tinymce',
+									'toolbar1'          => 'bold, italic, underline, |, bullist, numlist, |, justifyleft, justifycenter, justifyright, alignleft, aligncenter, alignright, |, link, unlink, |, pastetext, pasteword, removeformat, |, undo, redo',
+									'toolbar2'          => '',
+									'inline_styles'     => TRUE,
+									'relative_urls'     => FALSE,
+									'remove_linebreaks' => FALSE,
+									'plugins'           => implode( ',', $tinymcePlugins )
+								)
+							)
+						);
+
+						echo ob_get_clean();
+						?>
+					</div>
+
+					<?php
+					/**
+					 * Fires after the Edit Term form fields are displayed.
+					 *
+					 * The dynamic portion of the hook name, `$taxonomy`, refers to
+					 * the taxonomy slug.
+					 *
+					 * @since 3.0.0
+					 *
+					 * @param object $tag      Current taxonomy term object.
+					 * @param string $taxonomy Current taxonomy slug.
+					 */
+					do_action( "cn_{$taxonomy}_edit_form_fields", $term, $taxonomy );
+
+					/**
+					 * Fires at the end of the Edit Term form for all taxonomies.
+					 *
+					 * The dynamic portion of the hook name, `$taxonomy`, refers to the taxonomy slug.
+					 *
+					 * @since 3.0.0
+					 *
+					 * @param object $tag      Current taxonomy term object.
+					 * @param string $taxonomy Current taxonomy slug.
+					 */
+					do_action( "{$taxonomy}_edit_form", $term, $taxonomy );
 					?>
 
 					<input type="hidden" name="cn-action" value="update_category"/>
 
-					<p class="submit"><a class="button button-warning" href="admin.php?page=connections_categories"><?php _e( 'Cancel', 'connections' ); ?></a> <input class="button-primary" type="submit" value="<?php _e( 'Update Category', 'connections' ); ?>" name="update" class="button"/></p>
+					<p class="submit">
+						<a class="button button-warning" href="admin.php?page=connections_categories"><?php _e( 'Cancel', 'connections' ); ?></a>
+						<input type="submit" name="update" id="update" class="button button-primary" value="<?php _e( 'Update Category', 'connections' ); ?>"/>
+					</p>
 
 					<?php $form->close(); ?>
 
 				</div>
 			</div>
-			<?php
+		<?php
 		} else {
+
+			/**
+			 * @var CN_Terms_List_Table $table
+			 */
+			$table = cnTemplatePart::table( 'term-admin', array( 'screen' => get_current_screen()->id ) );
+			$table->prepare_items();
 			?>
 			<div class="wrap nosubsub">
 
-				<?php echo get_screen_icon( 'connections' ); ?>
-
 				<h2>Connections : <?php _e( 'Categories', 'connections' ); ?></h2>
+
+				<form class="search-form" action="" method="get">
+
+					<input type="hidden" name="page" value="<?php echo esc_attr( $_REQUEST['page'] ); ?>"/>
+					<?php $table->search_box( __( 'Search Categories', 'connections' ), 'category' ); ?>
+
+				</form>
+				<br class="clear"/>
 
 				<div id="col-container">
 
 					<div id="col-right">
 						<div class="col-wrap">
 							<?php
+
 							$attr = array(
 								'action' => '',
 								'method' => 'post'
 							);
 
 							$form->open( $attr );
-							$form->tokenField( 'bulk_delete_category' );
+							//$form->tokenField( 'bulk_delete_category' );
+							?>
+							<input type="hidden" name="cn-action" value="category_bulk_actions"/>
+							<?php
+							$table->display();
+
+							$form->close();
 
 							?>
 
-								<div class="tablenav">
-									<div class="alignleft actions">
-										<select name="action">
-											<option selected="selected" value=""><?php _e( 'Bulk Actions', 'connections' ); ?></option>
-											<option value="delete"><?php _e( 'Delete', 'connections' ); ?></option>
-										</select>
-										<input type="hidden" name="cn-action" value="category_bulk_actions"/>
-										<input type="submit" class="button-secondary action" value="<?php _e( 'Apply', 'connections' ); ?>"/>
-									</div>
-
-									<br class="clear"/>
-								</div>
-
-								<div class="clear"/></div>
-
-								<table cellspacing="0" class="widefat fixed">
-									<thead>
-										<tr>
-											<th class="manage-column column-cb check-column" id="cb" scope="col"><input type="checkbox"/></th>
-											<th class="manage-column column-name" id="name" scope="col"><?php _e( 'Name', 'connections' ); ?></th>
-											<th class="manage-column column-description" id="description" scope="col"><?php _e( 'Description', 'connections' ); ?></th>
-											<th class="manage-column column-slug" id="slug" scope="col"><?php _e( 'Slug', 'connections' ); ?></th>
-											<th class="manage-column column-posts" id="posts" scope="col"><?php _e( 'Info', 'connections' ); ?></th>
-										</tr>
-									</thead>
-
-									<tfoot>
-										<tr>
-											<th class="manage-column column-cb check-column" scope="col"><input type="checkbox"/></th>
-											<th class="manage-column column-name" scope="col"><?php _e( 'Name', 'connections' ); ?></th>
-											<th class="manage-column column-description" scope="col"><?php _e( 'Description', 'connections' ); ?></th>
-											<th class="manage-column column-slug" scope="col"><?php _e( 'Slug', 'connections' ); ?></th>
-											<th class="manage-column column-posts" scope="col"><?php _e( 'Info', 'connections' ); ?></th>
-										</tr>
-									</tfoot>
-
-									<tbody class="list:cat" id="the-list">
-
-										<?php echo $categoryObjects->buildCategoryRow( 'table', $connections->retrieve->categories() ); ?>
-
-									</tbody>
-								</table>
-
-							<?php $form->close(); ?>
+							<br class="clear" />
 
 							<script type="text/javascript">
 								/* <![CDATA[ */
-								(function($){
-									$(document).ready(function(){
-										$('#doaction, #doaction2').click(function(){
-											if ( $('select[name^="action"]').val() == 'delete' ) {
+								(function ($) {
+									$(document).ready(function () {
+										$('#doaction, #doaction2').click(function () {
+											if ($('select[name^="action"]').val() == 'delete') {
 												var m = 'You are about to delete the selected category(ies).\n  \'Cancel\' to stop, \'OK\' to delete.';
 												return showNotice.warn(m);
 											}
@@ -163,14 +292,45 @@ function connectionsShowCategoriesPage() {
 							</script>
 
 							<div class="form-wrap">
-								<p><?php _e( '<strong>Note:</strong><br/>Deleting a category which has been assigned to an entry will reassign that entry as <strong>Uncategorized</strong>.', 'connections' ); ?></p>
+								<p><?php _e(
+										'<strong>Note:</strong><br/>Deleting a category which has been assigned to an entry will reassign that entry as <strong>Uncategorized</strong>.',
+										'connections'
+									); ?></p>
 							</div>
 
+							<?php
+							/**
+							 * Fires after the taxonomy list table.
+							 *
+							 * The dynamic portion of the hook name, `$taxonomy`, refers to the taxonomy slug.
+							 *
+							 * @since 3.0.0
+							 *
+							 * @param string $taxonomy The taxonomy name.
+							 */
+							do_action( "cn_after-{$taxonomy}-table", $taxonomy );
+							?>
+
 						</div>
-					</div><!-- right column -->
+					</div>
+					<!-- right column -->
 
 					<div id="col-left">
 						<div class="col-wrap">
+
+							<?php
+							/**
+							 * Fires before the Add Term form for all taxonomies.
+							 *
+							 * The dynamic portion of the hook name, `$taxonomy`, refers to the taxonomy slug.
+							 *
+							 * @since 3.0.0
+							 *
+							 * @param string $taxonomy The taxonomy slug.
+							 */
+							do_action( "cn_{$taxonomy}_pre_add_form", $taxonomy );
+							?>
+
 							<div class="form-wrap">
 								<h3><?php _e( 'Add New Category', 'connections' ); ?></h3>
 
@@ -183,21 +343,172 @@ function connectionsShowCategoriesPage() {
 								$form->open( $attr );
 								$form->tokenField( 'add_category' );
 
-								$categoryObjects->showForm();
+								/**
+								 * Fires at the beginning of the Add Tag form.
+								 *
+								 * The dynamic portion of the hook name, `$taxonomy`, refers to the taxonomy slug.
+								 *
+								 * @since 3.7.0
+								 */
+								do_action( "cn_{$taxonomy}_term_new_form_tag" );
 								?>
+								<div class="form-field form-required term-name-wrap">
+									<label for="category_name"><?php _e( 'Name', 'connections' ); ?></label>
+									<input type="text" aria-required="true" size="40" value="" id="category_name" name="category_name"/>
+									<input type="hidden" value="" id="category_id" name="category_id"/>
+
+									<p><?php _e( 'The name is how it appears on your site.', 'connections' ); ?></p>
+								</div>
+
+								<div class="form-field term-slug-wrap">
+									<label for="category_slug"><?php _e( 'Slug', 'connections' ); ?></label>
+									<input type="text" size="40" value="" id="category_slug" name="category_slug"/>
+
+									<p><?php _e(
+											'The “slug” is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.',
+											'connections'
+										); ?></p>
+								</div>
+
+								<div class="form-field term-parent-wrap">
+									<label for="category_parent"><?php _e( 'Parent', 'connections' ); ?></label>
+
+									<?php
+									$dropdown_args = array(
+										'hide_empty'       => 0,
+										'hide_if_empty'    => FALSE,
+										'taxonomy'         => 'category',
+										'name'             => 'category_parent',
+										'orderby'          => 'name',
+										'hierarchical'     => TRUE,
+										'show_option_none' => __( 'None' ),
+									);
+
+									/**
+									 * Filter the taxonomy parent drop-down on the Edit Term page.
+									 *
+									 * @since 3.7.0
+									 *
+									 * @param array   $dropdown_args    {
+									 *                                  An array of taxonomy parent drop-down arguments.
+									 *
+									 * @type int|bool $hide_empty       Whether to hide terms not attached to any posts. Default 0|false.
+									 * @type bool     $hide_if_empty    Whether to hide the drop-down if no terms exist. Default false.
+									 * @type string   $taxonomy         The taxonomy slug.
+									 * @type string   $name             Value of the name attribute to use for the drop-down select element.
+									 *                                      Default 'parent'.
+									 * @type string   $orderby          The field to order by. Default 'name'.
+									 * @type bool     $hierarchical     Whether the taxonomy is hierarchical. Default true.
+									 * @type string   $show_option_none Label to display if there are no terms. Default 'None'.
+									 * }
+									 *
+									 * @param string  $taxonomy         The taxonomy slug.
+									 */
+									$dropdown_args = apply_filters(
+										'cn_taxonomy_parent_dropdown_args',
+										$dropdown_args,
+										'category'
+									);
+
+									cnTemplatePart::walker( 'term-select', $dropdown_args );
+									?>
+
+									<p><?php _e(
+											'Categories can have a hierarchy. You might have a Jazz category, and under that have children categories for Bebop and Big Band. Totally optional.',
+											'connections'
+										); ?></p>
+								</div>
+
+								<div class="form-field term-description-wrap">
+									<?php
+									ob_start();
+
+									/*
+									 * Now we're going to have to keep track of which TinyMCE plugins
+									 * WP core supports based on version, sigh.
+									 */
+									if ( version_compare( $GLOBALS['wp_version'], '3.8.999', '<' ) ) {
+
+										$tinymcePlugins = array(
+											'inlinepopups',
+											'tabfocus',
+											'paste',
+											'wordpress',
+											'wplink',
+											'wpdialogs'
+										);
+
+									} else {
+
+										$tinymcePlugins = array(
+											'tabfocus',
+											'paste',
+											'wordpress',
+											'wplink',
+											'wpdialogs'
+										);
+									}
+
+									wp_editor(
+										'',
+										'category_description',
+										array(
+											'media_buttons' => FALSE,
+											'tinymce'       => array(
+												'editor_selector'   => 'tinymce',
+												'toolbar1'          => 'bold, italic, underline, |, bullist, numlist, |, justifyleft, justifycenter, justifyright, alignleft, aligncenter, alignright, |, link, unlink, |, pastetext, pasteword, removeformat, |, undo, redo',
+												'toolbar2'          => '',
+												'inline_styles'     => TRUE,
+												'relative_urls'     => FALSE,
+												'remove_linebreaks' => FALSE,
+												'plugins'           => implode( ',', $tinymcePlugins )
+											)
+										)
+									);
+
+									echo ob_get_clean();
+									?>
+
+								</div>
 
 								<input type="hidden" name="cn-action" value="add_category"/>
-								<p class="submit"><input type="submit" value="<?php _e( 'Add New Category', 'connections' ); ?>" name="add" class="button"/></p>
+
+								<?php
+								/**
+								 * Fires after the Add Term form fields for hierarchical taxonomies.
+								 *
+								 * The dynamic portion of the hook name, `$taxonomy`, refers to the taxonomy slug.
+								 *
+								 * @since 3.0.0
+								 *
+								 * @param string $taxonomy The taxonomy slug.
+								 */
+								do_action( "cn_{$taxonomy}_add_form_fields", $taxonomy );
+
+								submit_button( __( 'Add New Category', 'connections' ), 'primary', 'add' );
+
+								/**
+								 * Fires at the end of the Add Term form for all taxonomies.
+								 *
+								 * The dynamic portion of the hook name, `$taxonomy`, refers to the taxonomy slug.
+								 *
+								 * @since 3.0.0
+								 *
+								 * @param string $taxonomy The taxonomy slug.
+								 */
+								do_action( "cn_{$taxonomy}_add_form", $taxonomy );
+								?>
 
 								<?php $form->close(); ?>
 							</div>
 						</div>
-					</div><!-- left column -->
+					</div>
+					<!-- left column -->
 
-				</div><!-- Column container -->
+				</div>
+				<!-- Column container -->
 			</div>
 		<?php
 		}
 	}
 }
-?>

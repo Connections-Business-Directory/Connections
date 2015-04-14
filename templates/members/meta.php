4f4b1fa6d78c549c7a08ed8f69a6308235b6d699
Steven A. Zahm <shazahm1@hotmail.com>
2015-04-14 18:32:12 -0400

    Run the Members template thru the phpStorm code formatter to get it much closer to following the coding style guidelines.

--- templates/members/meta.php
+++ templates/members/meta.php
@@ -5,23 +5,25 @@
  *
  * @package     Connections
  * @subpackage  Template Meta
- * @copyright   Copyright (c) 2013, Steven A. Zahm
+ * @copyright   Copyright (c) 2015, Steven A. Zahm
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       unknown
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
- * @author:   Phill Pafford
+ * @author :   Phill Pafford
  * @website:  http://llihp.blogspot.com
  *
- * @todo:
+ * @todo   :
  *    -Add Link to personal profile in popup for each member displayed, NOT FOR MOBILE VIEWING
  */
-$template->name = 'Members';
-$template->version = '1.0';
-$template->author = 'Phill Pafford';
+$template->name        = 'Members';
+$template->version     = '1.0';
+$template->author      = 'Phill Pafford';
 $template->description = 'Shows popup with a Google map';
-$template->type = 'family';
+$template->type        = 'family';

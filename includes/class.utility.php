080d62bc57a325ec174d34d9cb4c1eb82a188a34
Steven A. Zahm <shazahm1@hotmail.com>
2015-07-10 08:14:56 -0400

    Add whitespace to cnString::quickRandom() so it meets coding standards.

--- includes/class.utility.php
+++ includes/class.utility.php
@@ -1884,7 +1884,8 @@ class cnString {
 	public static function quickRandom( $length = 16 ) {
 
 		$pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		return substr(str_shuffle(str_repeat($pool, $length)), 0, $length);
+
+		return substr( str_shuffle( str_repeat( $pool, $length ) ), 0, $length );
 	}
 }
 

3908ca22f5380864bad30369df14ab0f3b494756
Steven A. Zahm <shazahm1@hotmail.com>
2015-09-17 15:20:40 -0400

    Update jQuery qTip to 2.2.1.

--- includes/class.scripts.php
+++ includes/class.scripts.php
@@ -179,7 +179,7 @@ class cnScript {
 			wp_register_script( 'cn-ui', $url . "assets/js/cn-user$min.js", array( 'jquery' ), CN_CURRENT_VERSION, $connections->options->getJavaScriptFooter() );
 		}
 
-		wp_register_script( 'jquery-qtip', $url . "vendor/jquery-qtip/jquery.qtip$min.js", array( 'jquery' ), '2.0.1', $connections->options->getJavaScriptFooter() );
+		wp_register_script( 'jquery-qtip', $url . "vendor/jquery-qtip/jquery.qtip$min.js", array( 'jquery' ), '2.2.1', $connections->options->getJavaScriptFooter() );
 
 		// Registering  with the handle 'jquery-chosen-min' for legacy support. Remove this at some point. 04/30/2014
 		wp_register_script( 'jquery-chosen', $url . "vendor/chosen/chosen.jquery$min.js", array( 'jquery' ), '1.4.2', $connections->options->getJavaScriptFooter() );
@@ -261,7 +261,7 @@ class cnScript {
 
 		}
 
-		wp_register_style( 'cn-qtip', $url . "vendor/jquery-qtip/jquery.qtip$min.css", array(), '2.0.1' );
+		wp_register_style( 'cn-qtip', $url . "vendor/jquery-qtip/jquery.qtip$min.css", array(), '2.2.1' );
 		wp_register_style( 'cn-chosen', $url . "vendor/chosen/chosen$min.css", array(), '1.4.2' );
 		wp_register_style( 'cn-font-awesome', $url . "vendor/font-awesome/css/font-awesome$min.css", array(), '4.4.0' );
 

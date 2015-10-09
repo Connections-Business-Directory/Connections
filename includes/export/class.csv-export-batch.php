fd240156b687fb41acd1a7aee70096c53241c7dd
Steven A. Zahm <shazahm1@hotmail.com>
2015-10-09 19:24:19 -0400

    Change usage of cnCSV_Export::addSlashesAndQuote() to cnCSV_Export::escapeAndQuote() throughout.

--- includes/export/class.csv-export-batch.php
+++ includes/export/class.csv-export-batch.php
@@ -176,7 +176,7 @@ class cnCSV_Batch_Export extends cnCSV_Export {
 	 */
 	public function writeHeaders() {
 
-		$headers = array_map( array( $this, 'addSlashesAndQuote' ), $this->getColumns() );
+		$headers = array_map( array( $this, 'escapeAndQuote' ), $this->getColumns() );
 		$headers = implode( ',', $headers ) . "\r\n";
 
 		$this->write( $headers );
@@ -192,7 +192,7 @@ class cnCSV_Batch_Export extends cnCSV_Export {
 	 *
 	 * @uses   cnCSV_Export::getData()
 	 * @uses   cnCSV_Export::getColumns()
-	 * @uses   cnCSV_Export::addSlashesAndQuote()
+	 * @uses   cnCSV_Export::escapeAndQuote()
 	 * @uses   cnCSV_Batch_Export::write()
 	 *
 	 * @return string|false
@@ -215,7 +215,7 @@ class cnCSV_Batch_Export extends cnCSV_Export {
 					// Make sure the column is valid.
 					if ( array_key_exists( $col_id, $cols ) ) {
 
-						$row_data .= $this->addSlashesAndQuote( $column );
+						$row_data .= $this->escapeAndQuote( $column );
 						$row_data .= $i == $count ? '' : ',';
 						$i++;
 					}

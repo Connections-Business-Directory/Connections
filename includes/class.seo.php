5bdffd2de6f3b449795b525ff98a8e50b714c8a3
Steven A. Zahm <shazahm1@hotmail.com>
2015-10-20 12:02:45 -0400

    Check for string length 0 to be more explicit in cnSEO::metaDesc().

--- includes/class.seo.php
+++ includes/class.seo.php
@@ -521,7 +521,7 @@ class cnSEO {
 			}
 		}
 
-		if ( empty( $description ) ) return;
+		if ( 0 == strlen( $description ) ) return;
 
 		echo '<meta name="description" content="' . esc_attr( trim( strip_shortcodes( strip_tags( stripslashes( $description ) ) ) ) ) . '"/>' . "\n";
 

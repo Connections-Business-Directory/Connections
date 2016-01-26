70c93725dd5bd4c1826092d6e69d5349a9a79af1
Steven A. Zahm <shazahm1@hotmail.com>
2016-01-26 11:54:35 -0500

    Tweak logic in cnUtility::getUUID() so if openssl_random_pseudo_bytes() fails, it'll gracefully fallback to the compatibility method rather than failing.

--- includes/class.utility.php
+++ includes/class.utility.php
@@ -1181,39 +1181,40 @@ class cnUtility {
 
 			$data    = openssl_random_pseudo_bytes(16);
 
-			$data[6] = chr( ord( $data[6] ) & 0x0f | 0x40 ); // set version to 0010
-			$data[8] = chr( ord( $data[8] ) & 0x3f | 0x80 ); // set bits 6-7 to 10
+			if ( FALSE !== $data ) {
 
-			return vsprintf( '%s%s-%s-%s-%s-%s%s%s', str_split( bin2hex( $data ), 4 ) );
+				$data[6] = chr( ord( $data[6] ) & 0x0f | 0x40 ); // set version to 0010
+				$data[8] = chr( ord( $data[8] ) & 0x3f | 0x80 ); // set bits 6-7 to 10
 
-		} else {
-
-			return sprintf(
-				'%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-
-				// 32 bits for "time_low"
-				mt_rand( 0, 0xffff ),
-				mt_rand( 0, 0xffff ),
-
-				// 16 bits for "time_mid"
-				mt_rand( 0, 0xffff ),
-
-				// 16 bits for "time_hi_and_version",
-				// four most significant bits holds version number 4
-				mt_rand( 0, 0x0fff ) | 0x4000,
-
-				// 16 bits, 8 bits for "clk_seq_hi_res",
-				// 8 bits for "clk_seq_low",
-				// two most significant bits holds zero and one for variant DCE1.1
-				mt_rand( 0, 0x3fff ) | 0x8000,
-
-				// 48 bits for "node"
-				mt_rand( 0, 0xffff ),
-				mt_rand( 0, 0xffff ),
-				mt_rand( 0, 0xffff )
-			);
+				return vsprintf( '%s%s-%s-%s-%s-%s%s%s', str_split( bin2hex( $data ), 4 ) );
+			}
 
 		}
+
+		return sprintf(
+			'%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+			// 32 bits for "time_low"
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff ),
+
+			// 16 bits for "time_mid"
+			mt_rand( 0, 0xffff ),
+
+			// 16 bits for "time_hi_and_version",
+			// four most significant bits holds version number 4
+			mt_rand( 0, 0x0fff ) | 0x4000,
+
+			// 16 bits, 8 bits for "clk_seq_hi_res",
+			// 8 bits for "clk_seq_low",
+			// two most significant bits holds zero and one for variant DCE1.1
+			mt_rand( 0, 0x3fff ) | 0x8000,
+
+			// 48 bits for "node"
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff )
+		);
 
 	}
 

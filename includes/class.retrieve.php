7e6a44c65b85a378944be9117be9b477482f62cd
Steven A. Zahm <shazahm1@hotmail.com>
2016-02-03 12:14:03 -0500

    Complete refactor of cnRetrieve::dates().

--- includes/class.retrieve.php
+++ includes/class.retrieve.php
@@ -2137,99 +2137,94 @@ class cnRetrieve {
 	/**
 	 * Returns as an array of objects containing the dates per the defined options.
 	 *
-	 * $atts['id'] (int) Retrieve the dates of the specified entry by entry id.
-	 * $atts['preferred'] (bool) Retrieve the preferred date; id must be supplied.
-	 * $atts['type'] (array) || (string) Retrieve specific date types, id must be supplied.
+	 * @param array $atts {
+	 *     Optional. An array of arguments.
 	 *
-	 * @param array   $suppliedAttr Accepted values as noted above.
-	 * @param bool    $returnData   Query just the entry IDs or not. If set to FALSE, only the entry IDs would be returned as an array. If set TRUE, the date data will be returned.
+	 *     @type int          $id        The entry ID in which to retrieve the dates for.
+	 *     @type bool         $preferred Whether or not to return only the preferred dates.
+	 *                                   Default: false
+	 *     @type array|string $type      The types to return.
+	 *                                   Default: array() which will return all registered types.
+	 *                                   Accepts: Any other registered types.
+	 *     @type int          $limit     The number to limit the results to.
+	 * }
 	 *
 	 * @return array
 	 */
-	public function dates( $suppliedAttr , $returnData = TRUE ) {
+	public static function dates( $atts = array() ) {
+
+		/** @var wpdb $wpdb */
+		global $wpdb;
+
+		$where = array( 'WHERE 1=1' );
+
+		$defaults = array(
+			'fields'    => 'all',
+			'id'        => NULL,
+			'preferred' => FALSE,
+			'type'      => array(),
+			'limit'     => NULL,
+		);
+
+		$atts = cnSanitize::args( $atts, $defaults );
 
 		/**
-		 * @var connectionsLoad $connections
-		 * @var wpdb $wpdb
+		 * @var string       $fields
+		 * @var int          $id
+		 * @var bool         $preferred
+		 * @var array|string $type
+		 * @var null|int     $limit
 		 */
-		global $wpdb, $connections;
-
-		$validate = new cnValidate();
-		$where[] = 'WHERE 1=1';
-
-		/*
-		 * // START -- Set the default attributes array. \\
-		 */
-		$defaultAttr['id'] = NULL;
-		$defaultAttr['preferred'] = NULL;
-		$defaultAttr['type'] = NULL;
-
-		$atts = $validate->attributesArray( $defaultAttr, $suppliedAttr );
-		/*
-		 * // END -- Set the default attributes array if not supplied. \\
-		 */
-
 		extract( $atts );
 
+		/*
+		 * Convert these to values to an array if they were supplied as a comma delimited string
+		 */
+		cnFunction::parseStringList( $type );
 
-		if ( ! empty( $id ) ) {
+		switch ( $atts['fields'] ) {
+
+			case 'ids':
+				$select = array( 'd.id', 'd.entry_id' );
+				break;
+
+			case 'date':
+				$select = array( 'd.date' );
+				break;
+
+			default:
+				$select = array( 'd.*' );
+		}
+
+		if ( is_numeric( $id ) && ! empty( $id ) ) {
+
 			$where[] = $wpdb->prepare( 'AND `entry_id` = "%d"', $id );
-
-			if ( ! empty( $preferred ) ) {
-				$where[] = $wpdb->prepare( 'AND `preferred` = %d', (bool) $preferred );
-			}
-
-			if ( ! empty( $type ) ) {
-				if ( ! is_array( $type ) ) $type = explode( ',' , trim( $type ) );
-
-				$where[] = stripslashes( $wpdb->prepare( 'AND `type` IN (\'%s\')', implode( "', '", (array) $type ) ) );
-			}
 		}
 
-		// Set query string for visibility based on user permissions if logged in.
-		if ( is_user_logged_in() ) {
-			if ( ! isset( $atts['visibility'] ) || empty( $atts['visibility'] ) ) {
-				if ( current_user_can( 'connections_view_public' ) ) $visibility[] = 'public';
-				if ( current_user_can( 'connections_view_private' ) ) $visibility[] = 'private';
-				if ( current_user_can( 'connections_view_unlisted' ) && is_admin() ) $visibility[] = 'unlisted';
-			}
-			else {
-				$visibility[] = $atts['visibility'];
-			}
-		}
-		else {
-			if ( $connections->options->getAllowPublic() ) $visibility[] = 'public';
-			if ( $atts['allow_public_override'] == TRUE && $connections->options->getAllowPublicOverride() ) $visibility[] = 'public';
-			if ( $atts['private_override'] == TRUE && $connections->options->getAllowPrivateOverride() ) $visibility[] = 'private';
+		if ( $preferred ) {
+
+			$where[] = $wpdb->prepare( 'AND `preferred` = %d', (bool) $preferred );
 		}
 
-		if ( ! empty( $visibility ) ) $where[] = 'AND `visibility` IN (\'' . implode( "', '", (array) $visibility ) . '\')';
+		if ( ! empty( $type ) ) {
 
-		if ( $returnData ) {
-			$sql = 'SELECT SQL_CALC_FOUND_ROWS DISTINCT ' . CN_ENTRY_DATE_TABLE . '.*
-
-					FROM ' . CN_ENTRY_DATE_TABLE . ' ' . ' ' .
-
-				implode( ' ', $where ) . ' ' .
-
-				'ORDER BY `order`';
-
-			//print_r($sql);
-
-			$results = $wpdb->get_results( $sql );
-		}
-		else {
-			$sql = 'SELECT SQL_CALC_FOUND_ROWS DISTINCT ' . CN_ENTRY_DATE_TABLE . '.entry_id
-
-					FROM ' . CN_ENTRY_DATE_TABLE . ' ' . ' ' . implode( ' ', $where );
-
-			//print_r($sql);
-			$results = $wpdb->get_col( $sql );
+			$where[] = $wpdb->prepare( 'AND `type` IN (' . cnFormatting::prepareINPlaceholders( $type ) . ')', $type );
 		}
 
-		if ( empty( $results ) ) return array();
+		$where = self::setQueryVisibility( $where, array( 'table' => 'd' ) );
 
-		//print_r($results);
+		$limit = is_null( $atts['limit'] ) ? '' : sprintf( ' LIMIT %d', $atts['limit'] );
+
+		$sql = sprintf(
+			'SELECT %1$s FROM %2$s AS d %3$s ORDER BY `order`%4$s',
+			implode( ', ', $select ),
+			CN_ENTRY_DATE_TABLE,
+			implode( ' ', $where ),
+			$limit
+		);
+
+		$results = $wpdb->get_results( $sql );
+
 		return $results;
 	}
 

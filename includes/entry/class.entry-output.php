6adeb2fd4c7f1c0d06d6eaa83080f3f0b9984fa0
Steven A. Zahm <shazahm1@hotmail.com>
2016-06-24 11:45:08 -0400

    Introduce the `cn_output_atts_map` filter.

--- includes/entry/class.entry-output.php
+++ includes/entry/class.entry-output.php
@@ -1339,9 +1339,6 @@ class cnOutput extends cnEntry {
 	 */
 	public function getMapBlock( $atts = array(), $cached = TRUE ) {
 
-		/*
-		 * // START -- Set the default attributes array. \\
-		 */
 		$defaults = array(
 			'preferred' => NULL,
 			'type'      => NULL,
@@ -1355,13 +1352,17 @@ class cnOutput extends cnEntry {
 			'return'    => FALSE,
 		);
 
-		$defaults = apply_filters( 'cn_output_default_atts_contact_name' , $defaults );
-
-		$atts = cnSanitize::args( $atts, $defaults );
-		$atts['id'] = $this->getId();
-		/*
-		 * // END -- Set the default attributes array if not supplied. \\
+		/**
+		 * All extensions to filter the method default and supplied args.
+		 *
+		 * @since 8.5.18
 		 */
+		$atts = cnSanitize::args(
+			apply_filters( 'cn_output_atts_map', $atts ),
+			apply_filters( 'cn_output_default_atts_map', $defaults )
+		);
+
+		$atts['id'] = $this->getId();
 
 		$out = '';
 		$attr = array();

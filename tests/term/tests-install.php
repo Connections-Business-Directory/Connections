c52d909d2e26544892e929bf032e26b8fcb4e7c8
Steven A. Zahm <shazahm1@hotmail.com>
2016-12-07 18:21:16 -0500

    PHPUnit: Rewrite test for Uncategorized category.

--- tests/term/tests-install.php
+++ tests/term/tests-install.php
@@ -8,16 +8,24 @@ class cnTests_Install extends WP_UnitTestCase {
 
 		$term = cnTerm::getBy( 'name', 'Uncategorized', 'category', 0 );
 
-		$this->assertEquals(
-			array(
-				'name' => 'Uncategorized',
-				'slug' => 'uncategorized',
-			),
-			array(
-				'name' => $term->name,
-				'slug' => $term->slug,
-			),
-			'Uncategorized category not found.'
-		);
+		if ( ! is_wp_error( $term ) ) {
+
+			$this->assertEquals(
+				array(
+					'name' => 'Uncategorized',
+					'slug' => 'uncategorized',
+				),
+				array(
+					'name' => $term->name,
+					'slug' => $term->slug,
+				),
+				'Uncategorized category not found.'
+			);
+
+		} else {
+
+			$this->assertTrue( FALSE, 'WP Error occurred. Uncategorized category not found.' );
+		}
+
 	}
 }

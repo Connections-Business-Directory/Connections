8353044f14f34266848e0652c3899df9e489f035
Steven A. Zahm <shazahm1@hotmail.com>
2016-12-07 18:20:40 -0500

    PHPUnit: Suppress $wpdb errors when loading plugins.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,7 +10,15 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
+
+	global $wpdb;
+	$wpdb->suppress_errors = TRUE;
+	$wpdb->show_errors = FALSE;
+
 	require dirname( __FILE__ ) . '/../connections.php';
+
+	$wpdb->suppress_errors = FALSE;
+	$wpdb->show_errors = TRUE;
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 

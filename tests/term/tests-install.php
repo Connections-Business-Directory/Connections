ed19b97b485fd0d644859126ab3aa7061a602748
Steven A. Zahm <shazahm1@hotmail.com>
2016-12-07 21:18:00 -0500

    PHPUnit: Remove core category test and uncomment the test code for the Uncategorized category.

--- tests/term/tests-install.php
+++ tests/term/tests-install.php
@@ -2,58 +2,28 @@
 
 class cnTests_Install extends WP_UnitTestCase {
 
-	/**
-	 * Validate get_category_by_slug function
-	 */
-	function test_get_category_by_slug() {
+	public function test_category_exists() {
 
-		// create Test Categories
-		$testcat = self::factory()->category->create_and_get(
-			array(
-				'slug' => 'testcat',
-				'name' => 'Test Category 1'
-			)
-		);
-		$testcat2 = self::factory()->category->create_and_get(
-			array(
-				'slug' => 'testcat2',
-				'name' => 'Test Category 2'
-			)
-		);
+		$term = cnTerm::getBy( 'name', 'Uncategorized', 'category' );
 
-		// validate category is returned by slug
-		$ret_testcat = get_category_by_slug( 'testcat' );
-		$this->assertEquals( $testcat->term_id, $ret_testcat->term_id );
-		$ret_testcat = get_category_by_slug( 'TeStCaT' );
-		$this->assertEquals( $testcat->term_id, $ret_testcat->term_id );
+		if ( ! is_wp_error( $term ) ) {
 
-		// validate unknown category returns false
-		$this->assertFalse( get_category_by_slug( 'testcat3' ) );
+			$this->assertEquals(
+				array(
+					'name' => 'Uncategorized',
+					'slug' => 'uncategorized',
+				),
+				array(
+					'name' => $term->name,
+					'slug' => $term->slug,
+				),
+				'Uncategorized category not found.'
+			);
+
+		} else {
+
+			$this->assertTrue( FALSE, 'WP Error occurred. Uncategorized category not found.' );
+		}
 
 	}
-
-	//public function test_category_exists() {
-	//
-	//	$term = cnTerm::getBy( 'name', 'Uncategorized', 'category', 0 );
-	//
-	//	if ( ! is_wp_error( $term ) ) {
-	//
-	//		$this->assertEquals(
-	//			array(
-	//				'name' => 'Uncategorized',
-	//				'slug' => 'uncategorized',
-	//			),
-	//			array(
-	//				'name' => $term->name,
-	//				'slug' => $term->slug,
-	//			),
-	//			'Uncategorized category not found.'
-	//		);
-	//
-	//	} else {
-	//
-	//		$this->assertTrue( FALSE, 'WP Error occurred. Uncategorized category not found.' );
-	//	}
-	//
-	//}
 }

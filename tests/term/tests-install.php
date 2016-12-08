af84a6f5d6c972fe0eadb7c10bb6f907fbba064e
Steven A. Zahm <shazahm1@hotmail.com>
2016-12-07 20:19:40 -0500

    PHPUnit: Add a core WP unit test... as a test to see if it passes without generating database errors.

--- tests/term/tests-install.php
+++ tests/term/tests-install.php
@@ -1,9 +1,37 @@
 <?php
 
 class cnTests_Install extends WP_UnitTestCase {
+
 	/**
-	 * @ticket 30975
+	 * Validate get_category_by_slug function
 	 */
+	function test_get_category_by_slug() {
+
+		// create Test Categories
+		$testcat = self::factory()->category->create_and_get(
+			array(
+				'slug' => 'testcat',
+				'name' => 'Test Category 1'
+			)
+		);
+		$testcat2 = self::factory()->category->create_and_get(
+			array(
+				'slug' => 'testcat2',
+				'name' => 'Test Category 2'
+			)
+		);
+
+		// validate category is returned by slug
+		$ret_testcat = get_category_by_slug( 'testcat' );
+		$this->assertEquals( $testcat->term_id, $ret_testcat->term_id );
+		$ret_testcat = get_category_by_slug( 'TeStCaT' );
+		$this->assertEquals( $testcat->term_id, $ret_testcat->term_id );
+
+		// validate unknown category returns false
+		$this->assertFalse( get_category_by_slug( 'testcat3' ) );
+
+	}
+
 	public function test_category_exists() {
 
 		$term = cnTerm::getBy( 'name', 'Uncategorized', 'category', 0 );

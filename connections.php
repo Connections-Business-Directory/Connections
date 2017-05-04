84412f1d40eaffaa54e9cdd68099ea4e542d2023
Steven A. Zahm <shazahm1@hotmail.com>
2017-05-04 15:09:11 -0400

    Bump version to 8.6.5.

--- connections.php
+++ connections.php
@@ -3,7 +3,7 @@
  * Plugin Name: Connections
  * Plugin URI: http://connections-pro.com/
  * Description: A business directory and address book manager.
- * Version: 8.6.4
+ * Version: 8.6.5
  * Author: Steven A. Zahm
  * Author URI: http://connections-pro.com/
  * Text Domain: connections
@@ -26,7 +26,7 @@
  * @package Connections
  * @category Core
  * @author Steven A. Zahm
- * @version 8.6.4
+ * @version 8.6.5
  */
 
 // Exit if accessed directly

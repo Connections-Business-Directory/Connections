d6b2c6bbdde3713844fed75750c0da8ed2cc4b25
Steven A. Zahm <shazahm1@hotmail.com>
2017-05-05 12:07:08 -0400

    Reset applied filters when when addresses to ensure all addresses are saved to the database.

--- includes/entry/address/class.entry-addresses.php
+++ includes/entry/address/class.entry-addresses.php
@@ -341,7 +341,7 @@ final class cnEntry_Addresses implements cnToArray {
 				'longitude'  => array( 'key' => 'longitude', 'format' => '%s' ),
 				'visibility' => array( 'key' => 'visibility', 'format' => '%s' ),
 			),
-			$this->getCollectionAsObjects(),
+			$this->resetFilters()->getCollectionAsObjects(),
 			array(
 				'id' => array( 'key' => 'id', 'format' => '%d' ),
 			)

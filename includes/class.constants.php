dbe2b1d72b059078fb72949a574c0642d5d65ce4
Steven A. Zahm <shazahm1@hotmail.com>
2017-05-22 16:23:27 -0400

    Bump version to 8.6.6.

--- includes/class.constants.php
+++ includes/class.constants.php
@@ -20,7 +20,7 @@ class cnConstants {
 		}
 
 		/** @var string CN_CURRENT_VERSION The current version. */
-		define( 'CN_CURRENT_VERSION', '8.6.5' );
+		define( 'CN_CURRENT_VERSION', '8.6.6' );
 
 		/** @var string CN_DB_VERSION The current DB version. */
 		define( 'CN_DB_VERSION', '0.4' );

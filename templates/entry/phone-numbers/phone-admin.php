c0ab4c59a79ffffd6db49cdc23997f291d48cd7d
Steven A. Zahm <shazahm1@hotmail.com>
2018-01-25 13:31:27 -0500

    Create the phone tempalte files for hCard and the manage admin page.

---
+++ templates/entry/phone-numbers/phone-admin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This is a copy/paste of the code which use to reside in manage.php file.
+ *
+ * @todo Clean so it is better "template" code.
+ *
+ * @var cnCollection $phoneNumbers
+ * @var cnPhone    $phone
+ */
+
+echo '<div class="phone-numbers">';
+
+foreach ( $phoneNumbers as $phone ) {
+	( $phone->preferred ) ? $preferred = '*' : $preferred = '';
+
+	echo '<span class="phone"><strong>' , $phone->name , '</strong>: ' ,  $phone->number , $preferred , '</span>';
+}
+
+echo '</div>';

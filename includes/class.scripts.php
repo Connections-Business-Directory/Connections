6a2d91c980bb93aeb04755a7153c83004156663d
Steven A. Zahm <shazahm1@hotmail.com>
2018-09-28 12:03:03 -0400

    Update Font Awesome to version 5.3.1.

--- includes/class.scripts.php
+++ includes/class.scripts.php
@@ -415,7 +415,7 @@ class cnScript {
 
 		wp_register_style( 'cn-qtip', $url . "vendor/jquery-qtip/jquery.qtip$min.css", array(), '2.2.1' );
 		wp_register_style( 'cn-chosen', $url . "vendor/chosen/chosen$min.css", array(), '1.8.7' );
-		wp_register_style( 'cn-font-awesome', $url . "vendor/font-awesome/css/font-awesome$min.css", array(), '4.4.0' );
+		wp_register_style( 'cn-font-awesome', $url . "vendor/font-awesome/css/all$min.css", array(), '5.3.1' );
 
 		// Remove the filter that adds the core CSS path to cnLocate.
 		remove_filter( 'cn_locate_file_paths', array( __CLASS__, 'coreCSSPath' ) );

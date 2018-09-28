e2cc80eeedad2d70589494109cfda3afaa80fe11
Steven A. Zahm <shazahm1@hotmail.com>
2018-09-28 11:00:18 -0400

    Remove references/includes to `class.upload.php`.

--- includes/php_class_upload/class.upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-// The only purpose of this file is to include class.upload.php so the CSV Import Extension does not break because the file path changed.
-
-require_once CN_PATH . 'vendor/php_class_upload/class.upload.php';

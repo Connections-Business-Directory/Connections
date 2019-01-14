fa9cb72ae4d8848ebea9f5eab5fb93b74a78f723
Steven A. Zahm <shazahm1@hotmail.com>
2019-01-14 14:04:51 -0500

    Add an Editor PageSelect component.

---
+++ includes/blocks/components/page-select.js
@@ -0,0 +1,105 @@
+/**
+ * External dependencies
+ */
+const { get } = lodash;
+
+/**
+ * WordPress dependencies
+ */
+const { __ } = wp.i18n;
+const { select, withSelect } = wp.data;
+const { Spinner } = wp.components;
+
+/**
+ * Internal dependencies
+ */
+const { TreeSelect } = wp.components;
+
+// Import components
+import {buildTermsTree} from '../utils/terms';
+
+function PageSelect( {
+	                     postType = 'page',
+	                     label,
+	                     value,
+	                     noOptionLabel,
+	                     options,
+	                     onChange,
+	                     ...props
+                     } ) {
+
+	if ( null === options ) {
+		return (
+			<p>
+				<Spinner />
+				{ __( 'Loading Data', 'connections' ) }
+			</p>
+		);
+	}
+
+	const { getPostType } = select( 'core' );
+
+	const postTypeMeta   = getPostType( postType );
+	const isHierarchical = get( postTypeMeta, [ 'hierarchical' ], false );
+	const pageItems      = options || [];
+	let   pagesTree      = [];
+
+	if ( ! pageItems.length ) {
+
+		return null;
+	}
+
+	if ( isHierarchical ) {
+
+		pagesTree = buildTermsTree( pageItems.map( ( item ) => ({
+			id:     item.id,
+			parent: item.parent,
+			name:   item.title.raw ? item.title.raw : `#${item.id} (${__( 'no title' )})`,
+		}) ) );
+
+	} else {
+
+		pagesTree = pageItems.map( ( item ) => ({
+			id:     item.id,
+			name:   item.title.raw ? item.title.raw : `#${item.id} (${__( 'no title' )})`,
+		}) );
+	}
+
+	return (
+		<TreeSelect
+			className="connections-directory--attributes__home_id"
+			label={label}
+			noOptionLabel={noOptionLabel}
+			tree={pagesTree}
+			selectedId={value}
+			onChange={onChange}
+			{...props}
+		/>
+	);
+}
+
+const applyWithSelect = withSelect( ( select, ownProps ) => {
+
+	const { getEntityRecords } = select( 'core' );
+	const { getCurrentPostId } = select( 'core/editor' );
+
+	const postType = typeof ownProps.postType === 'undefined' ? 'page' : ownProps.postType;
+	const postId   = getCurrentPostId();
+
+	const query = {
+		per_page:       -1,
+		exclude:        postId,
+		parent_exclude: postId,
+		orderby:        'title',
+		order:          'asc',
+	};
+
+	return {
+		options: getEntityRecords( 'postType', postType, query ),
+	};
+} );
+
+const renderedSelect = applyWithSelect( PageSelect );
+
+export { renderedSelect as PageSelect };
+// export default applyWithSelect( PageSelect );
---
+++ includes/blocks/utils/terms.js
@@ -0,0 +1,29 @@
+/**
+ * External dependencies
+ */
+const { groupBy } = lodash;
+
+/**
+ * Returns terms in a tree form.
+ *
+ * Copies from WP core since it does not appear to be available in the global wp object.
+ * @link https://github.com/WordPress/gutenberg/blob/master/packages/editor/src/utils/terms.js
+ *
+ * @param {Array} flatTerms  Array of terms in flat format.
+ *
+ * @return {Array} Array of terms in tree format.
+ */
+export function buildTermsTree( flatTerms ) {
+	const termsByParent = groupBy( flatTerms, 'parent' );
+	const fillWithChildren = ( terms ) => {
+		return terms.map( ( term ) => {
+			const children = termsByParent[ term.id ];
+			return {
+				...term,
+				children: children && children.length ? fillWithChildren( children ) : [],
+			};
+		} );
+	};
+
+	return fillWithChildren( termsByParent[ '0' ] || [] );
+}

886d739d4d7e8828c25b3e0428bddb84f7dde141
Steven A. Zahm <shazahm1@hotmail.com>
2019-05-02 17:02:13 -0400

    Bump version to 8.41.2.

--- includes/class.connections-directory.php
+++ includes/class.connections-directory.php
@@ -8,7 +8,7 @@ final class Connections_Directory {
 	 * The plugin version.
 	 * @since 8.16
 	 */
-	const VERSION = '8.41.1';
+	const VERSION = '8.41.2';
 
 	/**
 	 * Stores the instance of this class.

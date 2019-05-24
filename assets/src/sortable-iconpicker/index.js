d45c049267bc7a879062dc029e0882dab66aae74
Steven A. Zahm <shazahm1@hotmail.com>
2019-05-24 13:12:18 -0400

    Remove unused code from sortable-iconpicker.

--- assets/src/sortable-iconpicker/index.js
+++ assets/src/sortable-iconpicker/index.js
@@ -16,55 +16,6 @@ const e9_element = $( '#e9_element' ).fontIconPicker( {
 		theme:     'fip-darkgrey',
 	} );
 
-// // Add the event on the button
-// $( '#e9_buttons button' ).on( 'click', function( e ) {
-// 	e.preventDefault();
-//
-// 	// Show processing message
-// 	$( this ).prop( 'disabled', true ).html( '<i class="icon-cog demo-animate-spin"></i> Please wait...' );
-//
-// 	// Get the JSON file
-// 	$.ajax( {
-// 		url:      '//sandbox.connections-pro.com/wp-content/plugins/connections/assets/vendor/icomoon-brands/selection.json',
-// 		type:     'GET',
-// 		dataType: 'json'
-// 	} )
-// 		.done( function( response ) {
-//
-// 			// Get the class prefix
-// 			const classPrefix         = response.preferences.fontPref.prefix,
-// 			      icomoon_json_icons  = [],
-// 			      icomoon_json_search = [];
-//
-// 			// For each icon
-// 			$.each( response.icons, function( i, v ) {
-//
-// 				// Set the source
-// 				icomoon_json_icons.push( classPrefix + v.properties.name );
-//
-// 				// Create and set the search source
-// 				if ( v.icon && v.icon.tags && v.icon.tags.length ) {
-// 					icomoon_json_search.push( v.properties.name + ' ' + v.icon.tags.join( ' ' ) );
-// 				} else {
-// 					icomoon_json_search.push( v.properties.name );
-// 				}
-// 			} );
-//
-// 			console.log( icomoon_json_icons );
-//
-// 			// Set new fonts on fontIconPicker
-// 			e9_element.setIcons( icomoon_json_icons, icomoon_json_search );
-//
-// 			// Show success message and disable
-// 			$( '#e9_buttons button' ).removeClass( 'btn-primary' ).addClass( 'btn-success' ).text( 'Successfully loaded icons' ).prop( 'disabled', true );
-//
-// 		} )
-// 		.fail( function() {
-// 			// Show error message and enable
-// 			$( '#e9_buttons button' ).removeClass( 'btn-primary' ).addClass( 'btn-danger' ).text( 'Error: Try Again?' ).prop( 'disabled', false );
-// 		} );
-// 	e.stopPropagation();
-// } );
 
 const initModal = () => {
 

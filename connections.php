4803cfc117cdd286c2e5e9073e0b9765e53e7854
Steven A. Zahm <shazahm1@hotmail.com>
2020-02-17 16:30:45 -0500

    Bump version to 9.4.5.

--- connections.php
+++ connections.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Connections Business Directory
  * Plugin URI:        https://connections-pro.com/
  * Description:       A business directory and address book manager.
- * Version:           9.4.4
+ * Version:           9.4.5
  * Author:            Steven A. Zahm
  * Author URI:        https://connections-pro.com/
  * License:           GPL-2.0+
--- includes/class.connections-directory.php
+++ includes/class.connections-directory.php
@@ -8,7 +8,7 @@ final class Connections_Directory {
 	 * The plugin version.
 	 * @since 8.16
 	 */
-	const VERSION = '9.4.4';
+	const VERSION = '9.4.5';
 
 	/**
 	 * Stores the instance of this class.

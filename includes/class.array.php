6b485066c90dd339c9d71829892e636633139ac0
Steven A. Zahm <shazahm1@hotmail.com>
2020-09-15 12:38:55 -0400

    Add `_deprecated_file()` calls to a couple deprecated files.

--- includes/class.array.php
+++ includes/class.array.php
@@ -5,3 +5,5 @@
  * @link https://www.schmengler-se.de/en/2016/09/php-using-class_alias-to-maintain-bc-while-move-rename-classes/
  */
 class_exists( Connections_Directory\Utility\_array::class );
+
+_deprecated_file( basename( __FILE__ ), '9.11', 'includes/Utility/_array.php' );

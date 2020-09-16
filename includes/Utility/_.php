13aadc04cd26dc07702bcdb6b36f7ef0aba84ff5
Steven A. Zahm <shazahm1@hotmail.com>
2020-09-16 17:30:59 -0400

    Deprecate methods in the cnUtility class nad move code  to the _ class to consolidate utility methods under a shortcode classname.

--- includes/Utility/_.php
+++ includes/Utility/_.php
@@ -170,4 +170,174 @@ final class _ {
 		return $attr;
 	}
 
+	/**
+	 * Return a number only hash.
+	 *
+	 * @link https://stackoverflow.com/a/23679870/175071
+	 *
+	 * @since 9.8
+	 *
+	 * @param string   $str
+	 * @param int|null $len
+	 *
+	 * @return string
+	 */
+	public static function numHash( $str, $len = null ) {
+
+		$binhash = md5( $str, true );
+		$numhash = unpack( 'N2', $binhash );
+		$hash    = $numhash[1] . $numhash[2];
+
+		if ( $len !== null && is_int( $len ) ) {
+
+			$hash = substr( $hash, 0, $len );
+		}
+
+		return $hash;
+	}
+
+	/**
+	 * Get user IP.
+	 *
+	 * @since 0.8
+	 *
+	 * @return string The IP address.
+	 * @link   http://stackoverflow.com/a/6718472
+	 */
+	public static function getIP() {
+
+		foreach ( array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key ) {
+
+			if ( TRUE === array_key_exists( $key, $_SERVER ) ) {
+
+				foreach ( array_map( 'trim', explode( ',', $_SERVER[ $key ] ) ) as $ip ) {
+
+					if ( FALSE !== filter_var( $ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE ) ) {
+
+						return $ip;
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Returns v4 compliant UUID.
+	 *
+	 * @since 0.8
+	 *
+	 * @link http://www.php.net/manual/en/function.uniqid.php#94959
+	 * @link http://stackoverflow.com/a/15875555
+	 *
+	 * @return string
+	 */
+	public static function getUUID() {
+
+		if ( function_exists( 'openssl_random_pseudo_bytes' ) ) {
+
+			$data    = openssl_random_pseudo_bytes(16);
+
+			if ( FALSE !== $data ) {
+
+				$data[6] = chr( ord( $data[6] ) & 0x0f | 0x40 ); // set version to 0010
+				$data[8] = chr( ord( $data[8] ) & 0x3f | 0x80 ); // set bits 6-7 to 10
+
+				return vsprintf( '%s%s-%s-%s-%s-%s%s%s', str_split( bin2hex( $data ), 4 ) );
+			}
+
+		}
+
+		return sprintf(
+			'%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+			// 32 bits for "time_low"
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff ),
+
+			// 16 bits for "time_mid"
+			mt_rand( 0, 0xffff ),
+
+			// 16 bits for "time_hi_and_version",
+			// four most significant bits holds version number 4
+			mt_rand( 0, 0x0fff ) | 0x4000,
+
+			// 16 bits, 8 bits for "clk_seq_hi_res",
+			// 8 bits for "clk_seq_low",
+			// two most significant bits holds zero and one for variant DCE1.1
+			mt_rand( 0, 0x3fff ) | 0x8000,
+
+			// 48 bits for "node"
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff ),
+			mt_rand( 0, 0xffff )
+		);
+
+	}
+
+	/**
+	 * Convert a value within one range to a value within another range, maintaining ratio.
+	 *
+	 * Converted Python script from:
+	 * @link http://stackoverflow.com/a/15537393
+	 *
+	 * @since 8.1
+	 *
+	 * @param float $x    Original value.
+	 * @param float $oMin Old minimum.
+	 * @param float $oMax Old maximum.
+	 * @param float $nMin New minimum.
+	 * @param float $nMax New maximum.
+	 *
+	 * @return bool|float Return false on failure, or return new value within new range.
+	 */
+	public static function remapRange( $x, $oMin, $oMax, $nMin, $nMax ) {
+
+		#range check
+		if ( $oMin == $oMax ) {
+
+			return FALSE;
+		}
+
+		if ( $nMin == $nMax ) {
+
+			return FALSE;
+		}
+
+		#check reversed input range
+		$reverseInput = FALSE;
+		$oldMin       = min( $oMin, $oMax );
+		$oldMax       = max( $oMin, $oMax );
+
+		if ( ! $oldMin == $oMin ) {
+
+			$reverseInput = TRUE;
+		}
+
+		#check reversed output range
+		$reverseOutput = FALSE;
+		$newMin        = min( $nMin, $nMax );
+		$newMax        = max( $nMin, $nMax );
+
+		if ( ! $newMin == $nMin ) {
+
+			$reverseOutput = TRUE;
+		}
+
+		$portion = ( $x - $oldMin ) * ( $newMax - $newMin ) / ( $oldMax - $oldMin );
+
+		if ( $reverseInput ) {
+
+			$portion = ( $oldMax - $x ) * ( $newMax - $newMin ) / ( $oldMax - $oldMin );
+		}
+
+		$result = $portion + $newMin;
+
+		if ( $reverseOutput ) {
+
+			$result = $newMax - $portion;
+		}
+
+		return $result;
+	}
+
 }
--- includes/class.utility.php
+++ includes/class.utility.php
@@ -1,14 +1,6 @@
 <?php
 
-/**
- * String sanitation and validation.
- *
- * @package     Connections
- * @subpackage  Sanitation and Validation
- * @copyright   Copyright (c) 2013, Steven A. Zahm
- * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
- * @since       unknown
- */
+use Connections_Directory\Utility\_;
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
@@ -24,6 +16,7 @@ class cnUtility {
 	 * @link https://stackoverflow.com/a/23679870/175071
 	 *
 	 * @since 9.8
+	 * @deprecated 9.11
 	 *
 	 * @param string   $str
 	 * @param int|null $len
@@ -32,166 +25,67 @@ class cnUtility {
 	 */
 	public static function numHash( $str, $len = null ) {
 
-		$binhash = md5( $str, true );
-		$numhash = unpack( 'N2', $binhash );
-		$hash    = $numhash[1] . $numhash[2];
+		_deprecated_function( __METHOD__, '9.11', '_::numHash()' );
 
-		if ( $len !== null && is_int( $len ) ) {
-
-			$hash = substr( $hash, 0, $len );
-		}
-
-		return $hash;
+		return _::numHash( $str, $len );
 	}
 
 	/**
 	 * Get user IP.
 	 *
-	 * @access public
-	 * @since  0.8
-	 * @static
+	 * @since 0.8
+	 * @deprecated 9.11
 	 *
-	 * @link   http://stackoverflow.com/a/6718472
+	 * @link http://stackoverflow.com/a/6718472
 	 *
 	 * @return string The IP address.
 	 */
 	public static function getIP() {
 
-		foreach ( array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key ) {
+		_deprecated_function( __METHOD__, '9.11', '_::getIP()' );
 
-			if ( TRUE === array_key_exists( $key, $_SERVER ) ) {
-
-				foreach ( array_map( 'trim', explode( ',', $_SERVER[ $key ] ) ) as $ip ) {
-
-					if ( FALSE !== filter_var( $ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE ) ) {
-
-						return $ip;
-					}
-				}
-			}
-		}
+		return _::getIP();
 	}
 
 	/**
 	 * Returns v4 compliant UUID.
 	 *
-	 * @access public
-	 * @since  0.8
-	 * @static
+	 * @since 0.8
+	 * @deprecated 9.11
 	 *
-	 * @link   http://stackoverflow.com/a/15875555
-	 * @link   http://www.php.net/manual/en/function.uniqid.php#94959
+	 * @link http://stackoverflow.com/a/15875555
+	 * @link http://www.php.net/manual/en/function.uniqid.php#94959
 	 *
 	 * @return string
 	 */
 	public static function getUUID() {
 
-		if ( function_exists( 'openssl_random_pseudo_bytes' ) ) {
+		_deprecated_function( __METHOD__, '9.11', '_::getUUID()' );
 
-			$data    = openssl_random_pseudo_bytes(16);
-
-			if ( FALSE !== $data ) {
-
-				$data[6] = chr( ord( $data[6] ) & 0x0f | 0x40 ); // set version to 0010
-				$data[8] = chr( ord( $data[8] ) & 0x3f | 0x80 ); // set bits 6-7 to 10
-
-				return vsprintf( '%s%s-%s-%s-%s-%s%s%s', str_split( bin2hex( $data ), 4 ) );
-			}
-
-		}
-
-		return sprintf(
-			'%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-
-			// 32 bits for "time_low"
-			mt_rand( 0, 0xffff ),
-			mt_rand( 0, 0xffff ),
-
-			// 16 bits for "time_mid"
-			mt_rand( 0, 0xffff ),
-
-			// 16 bits for "time_hi_and_version",
-			// four most significant bits holds version number 4
-			mt_rand( 0, 0x0fff ) | 0x4000,
-
-			// 16 bits, 8 bits for "clk_seq_hi_res",
-			// 8 bits for "clk_seq_low",
-			// two most significant bits holds zero and one for variant DCE1.1
-			mt_rand( 0, 0x3fff ) | 0x8000,
-
-			// 48 bits for "node"
-			mt_rand( 0, 0xffff ),
-			mt_rand( 0, 0xffff ),
-			mt_rand( 0, 0xffff )
-		);
-
+		return _::getUUID();
 	}
 
 	/**
 	 * Convert a value within one range to a value within another range, maintaining ratio.
 	 *
 	 * Converted Python script from:
-	 * @link   http://stackoverflow.com/a/15537393
+	 * @link http://stackoverflow.com/a/15537393
 	 *
-	 * @access public
-	 * @since  8.1
-	 * @static
+	 * @since 8.1
+	 * @deprecated 9.11
 	 *
-	 * @param  float  $x    Original value.
-	 * @param  float  $oMin Old minimum.
-	 * @param  float  $oMax Old maximum.
-	 * @param  float  $nMin New minimum.
-	 * @param  float  $nMax New maximum.
+	 * @param float $x    Original value.
+	 * @param float $oMin Old minimum.
+	 * @param float $oMax Old maximum.
+	 * @param float $nMin New minimum.
+	 * @param float $nMax New maximum.
 	 *
-	 * @return mixed        bool | float Return false on failure, or return new value within new range.
+	 * @return bool|float Return false on failure, or return new value within new range.
 	 */
 	public static function remapRange( $x, $oMin, $oMax, $nMin, $nMax ) {
 
-		#range check
-		if ( $oMin == $oMax ) {
+		_deprecated_function( __METHOD__, '9.11', '_::remapRange()' );
 
-			return FALSE;
-		}
-
-		if ( $nMin == $nMax ) {
-
-			return FALSE;
-		}
-
-		#check reversed input range
-		$reverseInput = FALSE;
-		$oldMin       = min( $oMin, $oMax );
-		$oldMax       = max( $oMin, $oMax );
-
-		if ( ! $oldMin == $oMin ) {
-
-			$reverseInput = TRUE;
-		}
-
-		#check reversed output range
-		$reverseOutput = FALSE;
-		$newMin        = min( $nMin, $nMax );
-		$newMax        = max( $nMin, $nMax );
-
-		if ( ! $newMin == $nMin ) {
-
-			$reverseOutput = TRUE;
-		}
-
-		$portion = ( $x - $oldMin ) * ( $newMax - $newMin ) / ( $oldMax - $oldMin );
-
-		if ( $reverseInput ) {
-
-			$portion = ( $oldMax - $x ) * ( $newMax - $newMin ) / ( $oldMax - $oldMin );
-		}
-
-		$result = $portion + $newMin;
-
-		if ( $reverseOutput ) {
-
-			$result = $newMax - $portion;
-		}
-
-		return $result;
+		return _::remapRange( $x, $oMin, $oMax, $nMin, $nMax );
 	}
 }

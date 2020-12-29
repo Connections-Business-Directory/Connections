da08099ab2cea7513bfdeb0c859ee33291c5ee6b
Steven A. Zahm <shazahm1@hotmail.com>
2020-12-29 09:54:53 -0500

    Add deprecated SiteShot file for loading via legacy code. Reorder the class alias to prevent a PHP warning message.

---
+++ includes/SiteShot.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Trigger the autoloader for the `cnEntry_HTML` class using class_exists().
+ *
+ * @link https://www.schmengler-se.de/en/2016/09/php-using-class_alias-to-maintain-bc-while-move-rename-classes/
+ */
+class_exists( \Connections_Directory\Integration\WordPress\mShot::class );
+
+_deprecated_file( basename( __FILE__ ), '10.2', 'includes/Integration/WordPress/mShot.php' );

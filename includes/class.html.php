37daaf9d3de566391ad710a6c671a3075f296aa8
Steven A. Zahm <shazahm1@hotmail.com>
2021-02-02 15:01:49 -0500

    Correct `implode()` usage. Array must be 2nd parameter.

--- includes/class.html.php
+++ includes/class.html.php
@@ -411,7 +411,7 @@ class cnHTML {
 
 					array_walk( $value, 'esc_attr' );
 
-					return $value ? ' ' . esc_attr( $type ) . '="' . implode( $value, ' ' ) . '" ' : '';
+					return $value ? ' ' . esc_attr( $type ) . '="' . implode( ' ', $value ) . '" ' : '';
 
 				} elseif ( ! empty( $value ) ) {
 

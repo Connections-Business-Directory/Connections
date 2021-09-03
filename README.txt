fc8697e279d748d43576f0ed9627ff95bda69ccd
Steven A. Zahm <shazahm1@hotmail.com>
2021-09-03 15:24:27 -0400

    Bump version to 10.4.

--- README.txt
+++ README.txt
@@ -5,7 +5,7 @@ Tags: address book, business directory, chamber of commerce, church directory, c
 Requires at least: 5.1
 Tested up to: 5.8
 Requires PHP: 5.6.20
-Stable tag: 10.3.2
+Stable tag: 10.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- connections.php
+++ connections.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Connections Business Directory
  * Plugin URI:        https://connections-pro.com/
  * Description:       A business directory and address book manager.
- * Version:           10.3.2
+ * Version:           10.4
  * Author:            Steven A. Zahm
  * Author URI:        https://connections-pro.com/
  * License:           GPL-2.0+

8e710a39cecf7503a539eeb4fb4fa2403767259f
Steven A. Zahm <shazahm1@hotmail.com>
2021-09-14 12:24:01 -0400

    Bump version to 10.4.1.

--- includes/class.connections-directory.php
+++ includes/class.connections-directory.php
@@ -11,7 +11,7 @@ final class Connections_Directory {
 	 * The plugin version.
 	 * @since 8.16
 	 */
-	const VERSION = '10.4';
+	const VERSION = '10.4.1';
 
 	/**
 	 * Stores the instance of this class.

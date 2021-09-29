5ca036edb4c09e0a12cafd3ea458aec4e33b75a6
Steven A. Zahm <shazahm1@hotmail.com>
2021-09-29 16:01:22 -0400

    When a multi-item array uses associative keys, each value should start on a new line.

--- includes/admin/class.menu.php
+++ includes/admin/class.menu.php
@@ -77,13 +77,68 @@ class cnAdminMenu {
 			$instance->pageHook->topLevel = add_menu_page( 'Connections', 'Connections', 'connections_view_dashboard', 'connections_dashboard', array( __CLASS__, 'showPage' ), CN_URL . 'assets/images/menu.png', CN_ADMIN_MENU_POSITION );
 		}
 
-		$submenu[0]   = array( 'hook' => 'dashboard', 'page_title' => 'Connections : ' . __( 'Dashboard', 'connections' ), 'menu_title' => __( 'Dashboard', 'connections' ), 'capability' => 'connections_view_dashboard', 'menu_slug' => 'connections_dashboard', 'function' => array( __CLASS__, 'showPage' ) );
-		$submenu[20]  = array( 'hook' => 'manage', 'page_title' => 'Connections : ' . __( 'Manage', 'connections' ), 'menu_title' => __( 'Manage', 'connections' ), 'capability' => 'connections_manage', 'menu_slug' => 'connections_manage', 'function' => array( __CLASS__, 'showPage' ) );
-		$submenu[40]  = array( 'hook' => 'add', 'page_title' => 'Connections : ' . __( 'Add Entry', 'connections' ), 'menu_title' => __( 'Add Entry', 'connections' ), 'capability' => $addEntryCapability, 'menu_slug' => 'connections_add', 'function' => array( __CLASS__, 'showPage' ) );
-		$submenu[80]  = array( 'hook' => 'templates', 'page_title' => 'Connections : ' . __( 'Templates', 'connections' ), 'menu_title' => __( 'Templates', 'connections' ), 'capability' => 'connections_manage_template', 'menu_slug' => 'connections_templates', 'function' => array( __CLASS__, 'showPage' ) );
-		$submenu[100] = array( 'hook' => 'roles', 'page_title' => 'Connections : ' . __( 'Roles &amp; Capabilities', 'connections' ), 'menu_title' => __( 'Roles', 'connections' ), 'capability' => 'connections_change_roles', 'menu_slug' => 'connections_roles', 'function' => array( __CLASS__, 'showPage' ) );
-		$submenu[110] = array( 'hook' => 'tools', 'page_title' => 'Connections : ' . __( 'Tools', 'connections' ), 'menu_title' => __( 'Tools', 'connections' ), 'capability' => 'edit_posts', 'menu_slug' => 'connections_tools', 'function' => array( __CLASS__, 'showPage' ) );
-		$submenu[120] = array( 'hook' => 'settings', 'page_title' => 'Connections : ' . __( 'Settings', 'connections' ), 'menu_title' => __( 'Settings', 'connections' ), 'capability' => 'connections_change_settings', 'menu_slug' => 'connections_settings', 'function' => array( __CLASS__, 'showPage' ) );
+		$submenu[0] = array(
+			'hook'       => 'dashboard',
+			'page_title' => 'Connections : ' . __( 'Dashboard', 'connections' ),
+			'menu_title' => __( 'Dashboard', 'connections' ),
+			'capability' => 'connections_view_dashboard',
+			'menu_slug'  => 'connections_dashboard',
+			'function'   => array( __CLASS__, 'showPage' ),
+		);
+
+		$submenu[20] = array(
+			'hook'       => 'manage',
+			'page_title' => 'Connections : ' . __( 'Manage', 'connections' ),
+			'menu_title' => __( 'Manage', 'connections' ),
+			'capability' => 'connections_manage',
+			'menu_slug'  => 'connections_manage',
+			'function'   => array( __CLASS__, 'showPage' ),
+		);
+
+		$submenu[40] = array(
+			'hook'       => 'add',
+			'page_title' => 'Connections : ' . __( 'Add Entry', 'connections' ),
+			'menu_title' => __( 'Add Entry', 'connections' ),
+			'capability' => $addEntryCapability,
+			'menu_slug'  => 'connections_add',
+			'function'   => array( __CLASS__, 'showPage' ),
+		);
+
+		$submenu[80] = array(
+			'hook'       => 'templates',
+			'page_title' => 'Connections : ' . __( 'Templates', 'connections' ),
+			'menu_title' => __( 'Templates', 'connections' ),
+			'capability' => 'connections_manage_template',
+			'menu_slug'  => 'connections_templates',
+			'function'   => array( __CLASS__, 'showPage' ),
+		);
+
+		$submenu[100] = array(
+			'hook'       => 'roles',
+			'page_title' => 'Connections : ' . __( 'Roles &amp; Capabilities', 'connections' ),
+			'menu_title' => __( 'Roles', 'connections' ),
+			'capability' => 'connections_change_roles',
+			'menu_slug'  => 'connections_roles',
+			'function'   => array( __CLASS__, 'showPage' ),
+		);
+
+		$submenu[110] = array(
+			'hook'       => 'tools',
+			'page_title' => 'Connections : ' . __( 'Tools', 'connections' ),
+			'menu_title' => __( 'Tools', 'connections' ),
+			'capability' => 'edit_posts',
+			'menu_slug'  => 'connections_tools',
+			'function'   => array( __CLASS__, 'showPage' ),
+		);
+
+		$submenu[120] = array(
+			'hook'       => 'settings',
+			'page_title' => 'Connections : ' . __( 'Settings', 'connections' ),
+			'menu_title' => __( 'Settings', 'connections' ),
+			'capability' => 'connections_change_settings',
+			'menu_slug'  => 'connections_settings',
+			'function'   => array( __CLASS__, 'showPage' ),
+		);
 
 		$submenu = apply_filters( 'cn_submenu', $submenu );
 

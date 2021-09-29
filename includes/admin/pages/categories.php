eb96e17b397e003151b4105822ef64fcfa58ad02
Steven A. Zahm <shazahm1@hotmail.com>
2021-09-29 18:56:06 -0400

    phpcs:disable WordPress.Arrays.MultipleStatementAlignment.DoubleArrowNotAligned

--- includes/admin/pages/categories.php
+++ includes/admin/pages/categories.php
@@ -469,6 +469,7 @@ function connectionsShowCategoriesPage() {
 										);
 									}
 
+									// phpcs:disable WordPress.Arrays.MultipleStatementAlignment.DoubleArrowNotAligned
 									wp_editor(
 										'',
 										'category_description',
@@ -485,6 +486,7 @@ function connectionsShowCategoriesPage() {
 											),
 										)
 									);
+									// phpcs:enable
 
 									echo ob_get_clean(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 									?>

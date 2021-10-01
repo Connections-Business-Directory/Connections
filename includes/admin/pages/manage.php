90296d4915895e658c20413f5d0f6e254b853292
Steven A. Zahm <shazahm1@hotmail.com>
2021-10-01 12:35:53 -0400

    RUE, FALSE and NULL must be lowercase.

--- includes/admin/pages/manage.php
+++ includes/admin/pages/manage.php
@@ -229,10 +229,10 @@ function connectionsShowViewPage( $action = null ) {
 					echo '<div id="post-body" class="metabox-holder columns-' . ( 1 == get_current_screen()->get_columns() ? '1' : '2' ) . '">';
 
 						wp_nonce_field( 'cn-manage-metaboxes' );
-						wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', FALSE );
-						wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce', FALSE );
+						wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false );
+						wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce', false );
 
-						$form->tokenField( 'update_entry', FALSE, '_cn_wpnonce', FALSE );
+						$form->tokenField( 'update_entry', false, '_cn_wpnonce', false );
 
 						do_action( 'cn_admin_form_edit_entry_before', $entry, $form );
 

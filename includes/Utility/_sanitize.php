d9709509f4f2d3bd817dccdb8f9f1a8d662c45fc
Steven A. Zahm <shazahm1@hotmail.com>
2021-10-04 13:10:13 -0400

    Introduce `Utility\_sanitize`.

---
+++ includes/Utility/_sanitize.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Helper methods to sanitize user input.
+ *
+ * @package Connections_Directory\Utility
+ */
+
+namespace Connections_Directory\Utility;
+
+/**
+ * Class _sanitize
+ *
+ * @phpcs:disable PEAR.NamingConventions.ValidClassName.Invalid
+ * @phpcs:disable PEAR.NamingConventions.ValidClassName.StartWithCapital
+ */
+final class _sanitize {
+
+	/**
+	 * Sanitizes a string character.
+	 *
+	 * @since 10.4.4
+	 *
+	 * @param string $character The character.
+	 *
+	 * @return string Sanitized character.
+	 */
+	public static function character( $character ) {
+
+		if ( ! is_scalar( $character ) || ( ! empty( $character ) && strlen( $character ) > 1 ) ) {
+			$character = '';
+		}
+
+		return sanitize_text_field( $character );
+	}
+
+	/**
+	 * Sanitizes a string key.
+	 *
+	 * Keys are used as internal identifiers.
+	 * Alphanumeric characters, dashes, underscores, stops, colons and slashes are allowed.
+	 *
+	 * @since  10.4.4
+	 *
+	 * @param string $key String key.
+	 *
+	 * @return string Sanitized key.
+	 */
+	public static function key( $key ) {
+
+		$rawKey = $key;
+		$key    = preg_replace( '/[^a-zA-Z0-9_\-\.\:\/]/', '', $key );
+
+		/**
+		 * Filter a sanitized key string.
+		 *
+		 * @since 10.4.4
+		 *
+		 * @param string $key    Sanitized key.
+		 * @param string $rawKey The key prior to sanitization.
+		 */
+		return apply_filters( 'Connections_Directory/Utility/Sanitize/Key', $key, $rawKey );
+	}
+
+	/**
+	 * Sanitizes search term.
+	 *
+	 * @since 10.4.4
+	 *
+	 * @param string $term The search term to sanitize.
+	 *
+	 * @return string Sanitized search term.
+	 */
+	public static function search( $term ) {
+
+		// Fairly large, potentially too large, upper bound for search string lengths.
+		if ( ! is_scalar( $term ) || ( ! empty( $term ) && strlen( $term ) > 1600 ) ) {
+			$term = '';
+		}
+
+		return sanitize_text_field( $term );
+	}
+}

06a7f99a6029403b3712f2dead61102545db3909
Steven A. Zahm <shazahm1@hotmail.com>
2021-10-07 12:34:26 -0400

    Opening parenthesis of a multi-line function call must be the last content on the line.

--- includes/admin/pages/settings.php
+++ includes/admin/pages/settings.php
@@ -21,7 +21,8 @@ function connectionsShowSettingsPage() {
 	 */
 	if ( ! current_user_can( 'connections_change_settings' ) ) {
 
-		wp_die( '<p id="error-page" style="-moz-background-clip:border;
+		wp_die(
+			'<p id="error-page" style="-moz-background-clip:border;
 			-moz-border-radius:11px;
 			background:#FFFFFF none repeat scroll 0 0;
 			border:1px solid #DFDFDF;

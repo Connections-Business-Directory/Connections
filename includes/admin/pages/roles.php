2c210bc66a7228ebc773822b47b34b89929c0a37
Steven A. Zahm <shazahm1@hotmail.com>
2021-10-07 11:50:47 -0400

    Inline control structures are not allowed.

--- includes/admin/pages/roles.php
+++ includes/admin/pages/roles.php
@@ -91,7 +91,10 @@ function connectionsShowRolesPage() {
 							$name = translate_user_role( $details['name'] );
 
 							// The administrator should always have all capabilities.
-							if ( $role == 'administrator' ) continue;
+							if ( $role == 'administrator' ) {
+
+								continue;
+							}
 
 							$capabilies = cnRole::capabilities();
 

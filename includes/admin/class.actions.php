5c34bbb435bc64675a9e48563bd01bd140944df0
Steven A. Zahm <shazahm1@hotmail.com>
2021-10-11 14:52:56 -0400

    Use Yoda Condition checks, you must.

--- includes/admin/class.actions.php
+++ includes/admin/class.actions.php
@@ -1408,7 +1408,7 @@ class cnAdminActions {
 					}
 				}
 
-				if ( isset( $_POST['metakeyselect'] ) && $_POST['metakeyselect'] !== '-1' ) {
+				if ( isset( $_POST['metakeyselect'] ) && '-1' !== $_POST['metakeyselect'] ) {
 
 					$metaSelect[] = cnMeta::add( 'entry', $id, $_POST['metakeyselect'], $_POST['newmeta']['99']['value'] );
 				}

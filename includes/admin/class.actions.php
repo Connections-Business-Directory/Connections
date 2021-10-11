f4a48a759e3c560184a4b64b043798b64cc2d38b
Steven A. Zahm <shazahm1@hotmail.com>
2021-10-11 14:53:17 -0400

    phpcs:ignore WordPress.Security.NonceVerification.Missing

--- includes/admin/class.actions.php
+++ includes/admin/class.actions.php
@@ -1408,7 +1408,7 @@ class cnAdminActions {
 					}
 				}
 
-				if ( isset( $_POST['metakeyselect'] ) && '-1' !== $_POST['metakeyselect'] ) {
+				if ( isset( $_POST['metakeyselect'] ) && '-1' !== $_POST['metakeyselect'] ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
 
 					$metaSelect[] = cnMeta::add( 'entry', $id, $_POST['metakeyselect'], $_POST['newmeta']['99']['value'] );
 				}

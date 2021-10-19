c8616245ab755562f6363f8b14917950fb7bc633
Steven A. Zahm <shazahm1@hotmail.com>
2021-10-19 13:58:06 -0400

    Concat operator must be surrounded by a single space.

--- includes/template/class.template-walker-term-list.php
+++ includes/template/class.template-walker-term-list.php
@@ -184,7 +184,7 @@ class CN_Walker_Term_List extends Walker {
 
 			if ( ! empty( $atts['show_option_all'] ) ) {
 
-				$out .= '<li class="cat-item-all"><a href="' . cnURL::permalink( array( 'type' => 'home', 'data' => 'url', 'return' => true ) )  . '">' . $atts['show_option_all'] . '</a></li>';
+				$out .= '<li class="cat-item-all"><a href="' . cnURL::permalink( array( 'type' => 'home', 'data' => 'url', 'return' => true ) ) . '">' . $atts['show_option_all'] . '</a></li>';
 			}
 
 			$out .= $walker->walk( $terms, $atts['depth'], $atts );

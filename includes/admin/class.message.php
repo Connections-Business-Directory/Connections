a47923a4657e7b7f047e26066b8151e85da6386d
Steven A. Zahm <shazahm1@hotmail.com>
2021-10-21 12:34:22 -0400

    Use spaces, not tabs, for mid alignment.

--- includes/admin/class.message.php
+++ includes/admin/class.message.php
@@ -5,7 +5,7 @@
  *
  * @package     Connections
  * @subpackage  Messages
- * @uses		WP_Error
+ * @uses        WP_Error
  * @copyright   Copyright (c) 2013, Steven A. Zahm
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       0.7.5

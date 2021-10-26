7ba6dbb03c5743a011c3f1489839b29d2f640d5d
Steven A. Zahm <shazahm1@hotmail.com>
2021-10-26 14:19:04 -0400

    The CASE body must start on the line following the statement.

--- includes/shortcode/Conditional_Content.php
+++ includes/shortcode/Conditional_Content.php
@@ -249,12 +249,10 @@ class Conditional_Content extends cnShortcode {
 		switch ( $this->atts['condition'] ) {
 
 			case 'current_user_can':
-
 				$condition = $this->currentUserCan( $this->atts['parameter'] );
 				break;
 
 			case 'current_user_has_role':
-
 				$condition = $this->currentUserHasRole( $this->atts['parameter'] );
 				break;
 
@@ -267,7 +265,6 @@ class Conditional_Content extends cnShortcode {
 				break;
 
 			case 'is_front_page':
-
 				$condition = $this->isFrontPage();
 				break;
 
@@ -280,12 +277,10 @@ class Conditional_Content extends cnShortcode {
 				break;
 
 			case 'is_single':
-
 				$condition = $this->isSingle( $this->atts['parameter'] );
 				break;
 
 			case 'is_user_logged_in':
-
 				$condition = is_user_logged_in();
 				break;
 

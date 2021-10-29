66221e3fae0453def227d3ad05d8012ce0c70e90
Steven A. Zahm <shazahm1@hotmail.com>
2021-10-29 09:50:10 -0400

    Remove unnecessary parenthesis.

--- templates/entry/dates/date-hcard.php
+++ templates/entry/dates/date-hcard.php
@@ -25,10 +25,10 @@ foreach ( $dates as $date ) {
 	$row = "\t" . '<span class="vevent cn-date' . ( $date->preferred ? ' cn-preferred cn-date-preferred' : '' ) . '">';
 
 	// Hidden elements are to maintain hCalendar spec compatibility.
-	$replace[] = ( empty( $date->name ) ) ? '' : '<span class="date-name">' . $date->name . '</span>';
-	$replace[] = ( empty( $date->date ) ) ? '' : '<abbr class="dtstart" title="' . $date->date->format( 'Ymd' ) .'">' . date_i18n( $atts['date_format'], $date->date->getTimestamp(), false ) . '</abbr>
-                                                  <span class="summary" style="display:none">' . $date->name . ' - ' . $entry->getName( array( 'format' => $atts['name_format'] ) ) . '</span>
-                                                  <span class="uid" style="display:none">' . $date->date->format( 'YmdHis' ) . '</span>';
+	$replace[] = empty( $date->name ) ? '' : '<span class="date-name">' . $date->name . '</span>';
+	$replace[] = empty( $date->date ) ? '' : '<abbr class="dtstart" title="' . $date->date->format( 'Ymd' ) .'">' . date_i18n( $atts['date_format'], $date->date->getTimestamp(), false ) . '</abbr>
+                                              <span class="summary" style="display:none">' . $date->name . ' - ' . $entry->getName( array( 'format' => $atts['name_format'] ) ) . '</span>
+                                              <span class="uid" style="display:none">' . $date->date->format( 'YmdHis' ) . '</span>';
 	$replace[] = '<span class="cn-separator">' . $atts['separator'] . '</span>';
 
 	$row .= str_ireplace(

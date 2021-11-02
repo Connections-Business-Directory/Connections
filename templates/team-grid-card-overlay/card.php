058a45fa441952a942517485a17fe7652d9a6cb5
Steven A. Zahm <shazahm1@hotmail.com>
2021-11-02 13:20:13 -0400

    phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped

--- templates/team-grid-card-overlay/card.php
+++ templates/team-grid-card-overlay/card.php
@@ -56,7 +56,8 @@
 
 					if ( $number ) {
 
-						echo $number;
+						// Output is escaped in the `templates/entry/phone-numbers/phone-hcard.php` file.
+						echo $number; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 					} else {
 
@@ -82,7 +83,8 @@
 
 					if ( $email ) {
 
-						echo $email;
+						// Output is escaped in the `templates/entry/email-addresses/email-hcard.php` file.
+						echo $email; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 					} else {
 

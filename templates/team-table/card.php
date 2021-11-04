758dd56b17ad5f41e805ee7107b22431eaa9ea48
Steven A. Zahm <shazahm1@hotmail.com>
2021-11-04 13:13:03 -0400

    @phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound

--- templates/team-table/card.php
+++ templates/team-table/card.php
@@ -2,6 +2,8 @@
 /**
  * @var array    $atts
  * @var cnOutput $entry
+ *
+ * @phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
  */
 ?>
 <div class="cn-table-row cn-team-member">

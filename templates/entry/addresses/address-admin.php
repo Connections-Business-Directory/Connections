0d53132e0f7656d754cd51a7dbb20f911b6797eb
Steven A. Zahm <shazahm1@hotmail.com>
2021-11-05 15:08:49 -0400

    Escape admin address block template.

--- templates/entry/addresses/address-admin.php
+++ templates/entry/addresses/address-admin.php
@@ -15,55 +15,56 @@ foreach ( $addresses as $address ) {
 	( $address->preferred ) ? $preferred = '*' : $preferred = '';
 
 	if ( ! empty( $address->name ) ) {
-		echo '<span style="display: block"><strong>', $address->name, $preferred, '</strong></span>';
+		echo '<span style="display: block"><strong>', esc_html( "{$address->name}{$preferred}" ) , '</strong></span>';
 	}
 
 	if ( ! empty( $address->line_1 ) ) {
-		echo '<span style="display: block">', $address->line_1, '</span>';
+		echo '<span style="display: block">', esc_html( $address->line_1 ), '</span>';
 	}
 
 	if ( ! empty( $address->line_2 ) ) {
-		echo '<span style="display: block">', $address->line_2, '</span>';
+		echo '<span style="display: block">', esc_html( $address->line_2 ), '</span>';
 	}
 
 	if ( ! empty( $address->line_3 ) ) {
-		echo '<span style="display: block">', $address->line_3, '</span>';
+		echo '<span style="display: block">', esc_html( $address->line_3 ), '</span>';
 	}
 
 	if ( 0 < strlen( $address->line_4 ) ) {
-		echo '<span style="display: block">', $address->line_4, '</span>';
+		echo '<span style="display: block">', esc_html( $address->line_4 ), '</span>';
 	}
 
 	if ( 0 < strlen( $address->district ) ) {
-		echo '<span style="display: block">', $address->district, '</span>';
+		echo '<span style="display: block">', esc_html( $address->district ), '</span>';
 	}
 
 	if ( 0 < strlen( $address->county ) ) {
-		echo '<span style="display: block">', $address->county, '</span>';
+		echo '<span style="display: block">', esc_html( $address->county ), '</span>';
 	}
 
 	if ( ! empty( $address->city ) ) {
-		$outCache[] = '<span>' . $address->city . '</span>';
+		$outCache[] = '<span>' . esc_html( $address->city ) . '</span>';
 	}
 
 	if ( ! empty( $address->state ) ) {
-		$outCache[] = '<span>' . $address->state . '</span>';
+		$outCache[] = '<span>' . esc_html( $address->state ) . '</span>';
 	}
 
 	if ( ! empty( $address->zipcode ) ) {
-		$outCache[] = '<span>' . $address->zipcode . '</span>';
+		$outCache[] = '<span>' . esc_html( $address->zipcode ) . '</span>';
 	}
 
 	if ( ! empty( $outCache ) ) {
-		echo '<span style="display: block">', implode( '&nbsp;', $outCache ), '</span>';
+		// // HTML is escaped above.
+		echo '<span style="display: block">', implode( '&nbsp;', $outCache ), '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 
 	if ( ! empty( $address->country ) ) {
-		echo '<span style="display: block">', $address->country, '</span>';
+		echo '<span style="display: block">', esc_html( $address->country ), '</span>';
 	}
 
 	if ( ! empty( $address->latitude ) && ! empty( $address->longitude ) ) {
-		echo '<span style="display: block">', '<strong>', esc_html__( 'Latitude', 'connections' ), ':</strong>', ' ', $address->latitude, ' ', '<strong>', esc_html__( 'Longitude', 'connections' ), ':</strong>', ' ', $address->longitude, '</span>';
+		echo '<span style="display: block">', '<strong>', esc_html__( 'Latitude', 'connections' ), ':</strong>', ' ', esc_html( $address->latitude ), ' ', '<strong>', esc_html__( 'Longitude', 'connections' ), ':</strong>', ' ', esc_html( $address->longitude ), '</span>';
 	}
 
 	echo '</div>';

090071f693a74ad0fd88b16ff1f91065f233b0da
Steven A. Zahm <shazahm1@hotmail.com>
2021-11-05 13:55:54 -0400

    When a multi-item array uses associative keys, each value should start on a new line.

--- templates/team-grid-card-slide/card.php
+++ templates/team-grid-card-slide/card.php
@@ -36,13 +36,24 @@
 			<?php
 			if ( $atts['displayExcerpt'] ) {
 
-				$entry->excerpt( array( 'length' => absint( $atts['excerptWordLimit'] ), 'more' => '' ) );
+				$entry->excerpt(
+					array(
+						'length' => absint( $atts['excerptWordLimit'] ),
+						'more'   => '',
+					)
+				);
 			}
 
 			if ( $atts['displayPhone'] ) {
 
 				// $entry->getPhoneNumberBlock( array( 'format' => '%number%' ) );
-				$number = $entry->getPhoneNumberBlock( array( 'preferred' => true, 'format' => '%number%', 'return' => true ) );
+				$number = $entry->getPhoneNumberBlock(
+					array(
+						'preferred' => true,
+						'format'    => '%number%',
+						'return'    => true,
+					)
+				);
 
 				if ( $number ) {
 
@@ -50,14 +61,25 @@
 
 				} else {
 
-					$entry->getPhoneNumberBlock( array( 'format' => '%number%', 'limit' => 1 ) );
+					$entry->getPhoneNumberBlock(
+						array(
+							'format' => '%number%',
+							'limit'  => 1,
+						)
+					);
 				}
 			}
 
 			if ( $atts['displayEmail'] ) {
 
 				// $entry->getEmailAddressBlock( array( 'format' => '%address%' ) );
-				$email = $entry->getEmailAddressBlock( array( 'preferred' => true, 'format' => '%address%', 'return' => true ) );
+				$email = $entry->getEmailAddressBlock(
+					array(
+						'preferred' => true,
+						'format'    => '%address%',
+						'return'    => true,
+					)
+				);
 
 				if ( $email ) {
 
@@ -65,7 +87,12 @@
 
 				} else {
 
-					$entry->getEmailAddressBlock( array( 'format' => '%address%', 'limit' => 1 ) );
+					$entry->getEmailAddressBlock(
+						array(
+							'format' => '%address%',
+							'limit'  => 1,
+						)
+					);
 				}
 			}
 

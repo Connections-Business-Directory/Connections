afb2ac2649402d3371e5e5d8ceceafad75a1ae99
Steven A. Zahm <shazahm1@hotmail.com>
2021-11-05 13:47:49 -0400

    phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped

--- templates/team-list/card.php
+++ templates/team-list/card.php
@@ -50,7 +50,8 @@
 
 			if ( $number ) {
 
-				echo $number;
+				// Output is escaped in the `templates/entry/phone-numbers/phone-hcard.php` file.
+				echo $number; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 			} else {
 
@@ -76,7 +77,8 @@
 
 			if ( $email ) {
 
-				echo $email;
+				// Output is escaped in the `templates/entry/email-addresses/email-hcard.php` file.
+				echo $email; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 			} else {
 

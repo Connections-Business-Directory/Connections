5f2ea7cec35729b8a8f54773801396ed11fdc42a
Steven A. Zahm <shazahm1@hotmail.com>
2021-11-08 13:07:57 -0500

    Remove @todo.

--- templates/entry/social-networks/social-networks-admin.php
+++ templates/entry/social-networks/social-networks-admin.php
@@ -2,8 +2,6 @@
 /**
  * This is a copy/paste of the code which used to reside in manage.php file.
  *
- * @todo Clean so it is better "template" code.
- *
  * @var cnCollection           $networks
  * @var cnEntry_Social_Network $network
  */

939f002f34e7749e09b5ded131647128e04b7bfb
Steven A. Zahm <shazahm1@hotmail.com>
2021-11-08 13:12:04 -0500

    Escape admin social network block template.

--- templates/entry/social-networks/social-networks-admin.php
+++ templates/entry/social-networks/social-networks-admin.php
@@ -4,6 +4,8 @@
  *
  * @var cnCollection           $networks
  * @var cnEntry_Social_Network $network
+ *
+ * @phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
  */
 echo '<div class="social-networks">';
 
@@ -11,7 +13,7 @@ foreach ( $networks as $network ) {
 
 	$preferred = $network->preferred ? '*' : '';
 
-	echo '<span class="social-network"><strong>', $network->name, ':</strong> <a target="_blank" href="', $network->url, '">', $network->url . '</a>', $preferred, '</span>';
+	echo '<span class="social-network"><strong>' , esc_html( $network->name ) , ':</strong> <a target="_blank" href="' , esc_url( $network->url ) , '">' , esc_html( $network->url ) , '</a>', esc_html( $preferred ) , '</span>';
 }
 
 echo '</div>';

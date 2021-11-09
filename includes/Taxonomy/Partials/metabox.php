bd2cc1b2591fb5e24b3a4440a316ac51684703ae
Steven A. Zahm <shazahm1@hotmail.com>
2021-11-09 16:20:05 -0500

    All output should be run through an escaping function.

--- includes/Taxonomy/Partials/metabox.php
+++ includes/Taxonomy/Partials/metabox.php
@@ -34,7 +34,7 @@ if ( ! isset( $metabox['args'] ) || ! is_array( $metabox['args'] ) ) {
 }
 
 $parsed_args           = wp_parse_args( $args, $defaults );
-$tax_name              = esc_attr( $taxonomy->getSlug() );
+$tax_name              = $taxonomy->getSlug();
 $user_can_assign_terms = current_user_can( $taxonomy->getCapabilities()->assign_terms );
 $delimiter             = _x( ',', 'Term delimiter.', 'connections' );
 $terms_to_edit         = '';
@@ -58,16 +58,16 @@ if ( is_array( $terms ) ) {
 	$terms_to_edit = esc_attr( implode( ',', $term_names ) );
 }
 ?>
-<div class="tagsdiv" id="<?php echo $tax_name; ?>">
+<div class="tagsdiv" id="<?php echo esc_attr( $tax_name ); ?>">
 	<div class="jaxtag">
 		<div class="nojs-tags hide-if-js" style="display: block;">
-			<label for="tax-input-<?php echo $tax_name; ?>"><?php echo $taxonomy->getLabels()->add_or_remove_items; ?></label>
-			<p><textarea name="<?php echo $args['name']; ?>" rows="3" cols="20" class="the-tags" id="tax-input-<?php echo $tax_name; ?>" <?php disabled( ! $user_can_assign_terms ); ?> aria-describedby="new-tag-<?php echo $tax_name; ?>-desc"><?php echo str_replace( ',', $delimiter . ' ', $terms_to_edit ); // textarea_escaped by esc_attr() ?></textarea></p>
+			<label for="<?php echo esc_attr( "tax-input-{$tax_name}" ); ?>"><?php echo esc_html( $taxonomy->getLabels()->add_or_remove_items ); ?></label>
+			<p><textarea name="<?php echo esc_attr( $args['name'] ); ?>" rows="3" cols="20" class="the-tags" id="<?php echo esc_attr( "tax-input-{$tax_name}" ); ?>" <?php disabled( ! $user_can_assign_terms ); ?> aria-describedby="<?php echo esc_attr( "new-tag-{$tax_name}" ); ?>-desc"><?php echo str_replace( ',', $delimiter . ' ', $terms_to_edit ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></textarea></p>
 		</div>
 		<?php if ( $user_can_assign_terms ) : ?>
-			<p class="howto" id="new-tag-<?php echo $tax_name; ?>-desc"><?php echo $taxonomy->getLabels()->separate_items_with_commas; ?></p>
+			<p class="howto" id="<?php echo esc_attr( "new-tag-{$tax_name}-desc" ); ?>"><?php echo esc_html( $taxonomy->getLabels()->separate_items_with_commas ); ?></p>
 		<?php elseif ( empty( $terms_to_edit ) ) : ?>
-			<p><?php echo $taxonomy->getLabels()->no_terms; ?></p>
+			<p><?php echo esc_html( $taxonomy->getLabels()->no_terms ); ?></p>
 		<?php endif; ?>
 	</div>
 </div>

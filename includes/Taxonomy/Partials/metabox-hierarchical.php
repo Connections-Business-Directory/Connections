bb759015b4599d7fd635fbd763ca3ef482bcefab
Steven A. Zahm <shazahm1@hotmail.com>
2021-11-09 16:21:05 -0500

    phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound

--- includes/Taxonomy/Partials/metabox-hierarchical.php
+++ includes/Taxonomy/Partials/metabox-hierarchical.php
@@ -17,6 +17,8 @@
  *     @type callable $callback Metabox display callback.
  *     @type array    $args     Extra meta box arguments.
  * }
+ *
+ * @phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
  */
 
 $defaults = array(

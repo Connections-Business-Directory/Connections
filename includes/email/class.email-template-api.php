fcde4869ad75ff863ebe034089ba044e3169cbb4
Steven A. Zahm <shazahm1@hotmail.com>
2021-11-11 12:47:45 -0500

    Remove HTML inspection flags.

--- includes/email/class.email-template-api.php
+++ includes/email/class.email-template-api.php
@@ -271,9 +271,7 @@ class cnEmail_Template {
 		ob_start();
 		?>
 
-		<!--suppress HtmlRequiredLangAttribute -->
 		<html>
-		<!--suppress HtmlRequiredTitleElement -->
 		<head>
 			<style type="text/css">#outlook a { padding: 0; }</style>
 		</head>

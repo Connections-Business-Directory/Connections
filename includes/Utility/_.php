3479b73b063fc5eeb3bed7b12a14ecfc21c00bed
Steven A. Zahm <shazahm1@hotmail.com>
2021-11-24 14:44:49 -0500

    phpcs:disable PEAR.NamingConventions.ValidClassName.Invalid phpcs:disable PEAR.NamingConventions.ValidClassName.StartWithCapital

--- includes/Utility/_.php
+++ includes/Utility/_.php
@@ -11,6 +11,9 @@ use function Connections_Directory\Utility\_deprecated\_func as _deprecated_func
  * Class _
  *
  * @package Connections_Directory\Utility
+ *
+ * @phpcs:disable PEAR.NamingConventions.ValidClassName.Invalid
+ * @phpcs:disable PEAR.NamingConventions.ValidClassName.StartWithCapital
  */
 final class _ {
 

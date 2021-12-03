5ddd4a2fa429d41aa1d17d656eaf2eba096f6056
Steven A. Zahm <shazahm1@hotmail.com>
2021-12-03 13:08:41 -0500

    Inline comments must end in full-stops, exclamation marks, or question marks.

--- includes/inc.processes.php
+++ includes/inc.processes.php
@@ -11,7 +11,7 @@
 
 use function Connections_Directory\Utility\_deprecated\_func as _deprecated_function;
 
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }

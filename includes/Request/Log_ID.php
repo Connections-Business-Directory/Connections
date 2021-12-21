05af532d21742e1e779ba5eee8b6b7a4b76f1b68
Steven A. Zahm <shazahm1@hotmail.com>
2021-12-21 11:05:30 -0500

    Introduce `Request\Log_ID` and `Request\Log_Type`.

---
+++ includes/Request/Log_ID.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Get, validate, and validate log ID request variables.
+ *
+ * @since 10.4.8
+ *
+ * @category   WordPress\Plugin
+ * @package    Connections Business Directory
+ * @subpackage Connections\Request\Log ID
+ * @author     Steven A. Zahm
+ * @license    GPL-2.0+
+ * @copyright  Copyright (c) 2021, Steven A. Zahm
+ * @link       https://connections-pro.com/
+ */
+
+namespace Connections_Directory\Request;
+
+/**
+ * Class Log_ID
+ *
+ * @package Connections_Directory\Request
+ */
+class Log_ID extends Input {
+
+	/**
+	 * The request variable key.
+	 *
+	 * @since 10.4.8
+	 *
+	 * @var string
+	 */
+	protected $key = 'log_id';
+
+	/**
+	 * The input scheme.
+	 *
+	 * @since 10.4.8
+	 *
+	 * @var array
+	 */
+	protected $schema = array(
+		'default' => 0,
+		'type'    => 'integer',
+	);
+
+	/**
+	 * Sanitize the Log ID.
+	 *
+	 * @since 10.4.8
+	 *
+	 * @param string $unsafe The value to sanitize.
+	 *
+	 * @return int
+	 */
+	protected function sanitize( $unsafe ) {
+
+		return absint( $unsafe );
+	}
+
+	/**
+	 * Validate the Log ID.
+	 *
+	 * This is sufficiently validated against the schema, return `true`.
+	 *
+	 * @since 10.4.8
+	 *
+	 * @param string $unsafe The raw request value to validate.
+	 *
+	 * @return true
+	 */
+	protected function validate( $unsafe ) {
+
+		return true;
+	}
+}

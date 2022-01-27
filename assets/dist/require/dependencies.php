0ed1a5fc906ab4175ac5df164f1b2cec0abd6979
Steven A. Zahm <shazahm1@hotmail.com>
2022-01-27 12:53:29 -0500

    Update `dist` files.

---
+++ assets/dist/require/dependencies.php
@@ -0,0 +1,68 @@
+<?php
+return array(
+	'admin/icon-picker/script.js' => array(
+		'dependencies' => array( 'jquery' ),
+		'version'      => '8f1eacd9a2d73db8c49576415c26153e',
+	),
+	'admin/style.js' => array(
+		'dependencies' => array(),
+		'version'      => 'da92e8f1cfcb970c4bba9268f134ac30',
+	),
+	'frontend/style.js' => array(
+		'dependencies' => array(),
+		'version'      => '11e770e712f6ae9176a301a66d4bcc19',
+	),
+	'block/editor/style.js' => array(
+		'dependencies' => array(),
+		'version'      => 'fd93afba00bf9602e1fc0562bd946e1c',
+	),
+	'block/editor/script.js' => array(
+		'dependencies' => array(
+			'lodash',
+			'react',
+			'wp-api-fetch',
+			'wp-block-editor',
+			'wp-blocks',
+			'wp-components',
+			'wp-compose',
+			'wp-data',
+			'wp-element',
+			'wp-html-entities',
+			'wp-i18n',
+			'wp-polyfill',
+			'wp-server-side-render',
+			'wp-url',
+		),
+		'version'      => '2debfaf6cacb3ef7c630cec3a57001f2',
+	),
+	'block/carousel/script.js' => array(
+		'dependencies' => array(
+			'react',
+			'wp-element',
+			'wp-html-entities',
+		),
+		'version'      => '34bc3e0d9cdef2c284dbe5a8c3c9f658',
+	),
+	'block/carousel/style.js' => array(
+		'dependencies' => array(),
+		'version'      => 'b3379c03496e4deccee50f2bc4ebb418',
+	),
+	'block/team/style.js' => array(
+		'dependencies' => array(),
+		'version'      => '62a11b45668a488f1cb91cf0c3e89b5f',
+	),
+	'content-block/recently-viewed/script.js' => array(
+		'dependencies' => array(
+			'lodash',
+			'wp-api-fetch',
+			'wp-components',
+			'wp-compose',
+			'wp-data',
+			'wp-element',
+			'wp-html-entities',
+			'wp-i18n',
+			'wp-url',
+		),
+		'version'      => '59a8cf52953c95eeeaaffb319703300d',
+	),
+);

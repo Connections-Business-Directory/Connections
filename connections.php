d7a97a8613f125970bbde07ee30c0acd4a13838e
Steven A. Zahm <shazahm1@hotmail.com>
2022-02-07 13:05:39 -0500

    Drop max supported PHP version to 8.0.

--- connections.php
+++ connections.php
@@ -48,7 +48,7 @@ $check = new cnRequirements_Check(
 		'requirements' => array(
 			'php' => array(
 				'min' => '5.6.20', // 5.6.20 -- The minimum PHP version that WordPress 5.2 requires.
-				'max' => '8.1', // 7.4
+				'max' => '8.0', // 7.4
 			),
 			'wp'  => array(
 				'min' => '5.3', // 4.7.12

1873a47eef208de14fe18404b650cea8697a5543
Steven A. Zahm <shazahm1@hotmail.com>
2022-02-18 13:55:49 -0500

    Bump version to 10.4.15.

--- README.txt
+++ README.txt
@@ -5,7 +5,7 @@ Tags: business directory, directory plugin, member directory, listings, staff di
 Requires at least: 5.3
 Tested up to: 5.9
 Requires PHP: 5.6.20
-Stable tag: 10.4.14
+Stable tag: 10.4.15
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- connections.php
+++ connections.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Connections Business Directory
  * Plugin URI:        https://connections-pro.com/
  * Description:       A business directory and address book manager.
- * Version:           10.4.14
+ * Version:           10.4.15
  * Requires at least: 5.3
  * Requires PHP:      5.6.20
  * Author:            Steven A. Zahm

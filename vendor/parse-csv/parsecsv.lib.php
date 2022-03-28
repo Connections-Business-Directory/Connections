77a85bac116970300bcf0d9144d95d9bbe6d760d
Steven A. Zahm <shazahm1@hotmail.com>
2022-03-28 14:59:39 -0400

    Add placeholder files in the composer vendor folder that load the `parseCSV` library from Libraries folder.

---
+++ vendor/parse-csv/parsecsv.lib.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The `parseCSV` class has been moved to the `/includes/Libraries/parseCSV` subdirectory.
+ */
+
+use function Connections_Directory\Utility\_deprecated\_file as _deprecated_file;
+
+_deprecated_file(
+	basename( __FILE__ ),
+	'10.4.18',
+	CN_PATH . 'includes/Libraries/parseCSV/parsecsv.lib.php',
+	__( 'This class has been moved to Libraries/parseCSV subdirectory.', 'connections' )
+);
+
+require_once CN_PATH . 'includes/Libraries/parseCSV/parsecsv.lib.php';

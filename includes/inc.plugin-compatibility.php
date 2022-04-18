09065f274def9843f735d92721d38d625b50cff4
Steven A. Zahm <shazahm1@hotmail.com>
2022-04-18 10:07:44 -0400

    Remove unused commented out code.

--- includes/inc.plugin-compatibility.php
+++ includes/inc.plugin-compatibility.php
@@ -55,26 +55,6 @@ function cn_upload_size_limit( $bytes ) {
 }
 add_filter( 'upload_size_limit', 'cn_upload_size_limit' );
 
-///**
-// * Ensure WPSEO header items are not added to internal Connections pages.
-// * @todo Should add Connections related header items to mimic WPSEO.
-// *
-// * @access private
-// * @since  8.1.1
-// * @return void
-// */
-//function cn_remove_wpseo_head() {
-//
-//	if ( cnQuery::getVar( 'cn-entry-slug' ) ||
-//		 cnQuery::getVar( 'cn-cat-slug' ) ||
-//		 cnQuery::getVar( 'cn-cat' ) ) {
-//
-//		if ( isset( $GLOBALS['wpseo_front'] ) ) remove_action( 'wp_head', array( $GLOBALS['wpseo_front'], 'head' ), 1 );
-//	}
-//
-//}
-//add_action( 'parse_query', 'cn_remove_wpseo_head' );
-
 /**
  * Prevent s2member re-setting custom capabilities on re-activation.
  * gh-392
@@ -416,7 +396,7 @@ add_filter(
 /**
  * Compatibility with the Post Categories by User for WordPress plugin.
  *
- * Prevent this plugin from hiding the categories on the Connections admin pages.
+ * Prevent this plugin from hiding the categories on the Connections' admin pages.
  *
  * @link https://codecanyon.net/item/post-categories-by-user-for-wordpress/9958036
  * @since 10.2

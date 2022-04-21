d67ef2b85f4a40ba7188a2c3076ea6a7998f0127
Steven A. Zahm <shazahm1@hotmail.com>
2022-04-21 10:31:32 -0400

    Bump version to 10.4.21.

--- connections.php
+++ connections.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Connections Business Directory
  * Plugin URI:        https://connections-pro.com/
  * Description:       A business directory and address book manager.
- * Version:           10.4.21
+ * Version:           10.4.22
  * Requires at least: 5.4
  * Requires PHP:      5.6.20
  * Author:            Steven A. Zahm

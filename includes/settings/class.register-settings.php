e2b93630520eb97f1dd3ec4e0b3342b5602c888f
Steven A. Zahm <shazahm1@hotmail.com>
2022-05-03 16:33:40 -0400

    Update URL from `http` to `https`.

--- includes/settings/class.register-settings.php
+++ includes/settings/class.register-settings.php
@@ -5,7 +5,7 @@
  * @package     Connections
  * @subpackage  Manage the settings.
  * @copyright   Copyright (c) 2013, Steven A. Zahm
- * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @license     https://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       0.7.3.0
  */
 

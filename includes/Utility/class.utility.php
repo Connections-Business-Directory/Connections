f48e633244c11efad976cf76c4586d8a3b339887
Steven A. Zahm <shazahm1@hotmail.com>
2022-08-10 14:52:17 -0400

    Remove deprecated methods from `cnUtility`.

--- includes/Utility/class.utility.php
+++ includes/Utility/class.utility.php
@@ -15,44 +15,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 class cnUtility {
 
 	/**
-	 * Return a number only hash.
-	 *
-	 * @link https://stackoverflow.com/a/23679870/175071
-	 *
-	 * @since 9.8
-	 * @deprecated 9.11
-	 *
-	 * @param string   $str
-	 * @param int|null $len
-	 *
-	 * @return string
-	 */
-	public static function numHash( $str, $len = null ) {
-
-		_deprecated_function( __METHOD__, '9.11', '_string::toNumericHash()' );
-
-		return _string::toNumericHash( $str, $len );
-	}
-
-	/**
-	 * Get user IP.
-	 *
-	 * @since 0.8
-	 * @deprecated 9.11
-	 *
-	 * @link http://stackoverflow.com/a/6718472
-	 *
-	 * @return string The IP address.
-	 */
-	public static function getIP() {
-
-		_deprecated_function( __METHOD__, '9.11', '_::getIP()' );
-
-		return _::getIP();
-	}
-
-	/**
 	 * Returns v4 compliant UUID.
+	 *
+	 * NOTE: This method may still be in used in older versions of the CSV Import addon.
 	 *
 	 * @since 0.8
 	 * @deprecated 9.11
@@ -67,29 +32,5 @@ class cnUtility {
 		_deprecated_function( __METHOD__, '9.11', '_::getUUID()' );
 
 		return _::getUUID();
-	}
-
-	/**
-	 * Convert a value within one range to a value within another range, maintaining ratio.
-	 *
-	 * Converted Python script from:
-	 * @link http://stackoverflow.com/a/15537393
-	 *
-	 * @since 8.1
-	 * @deprecated 9.11
-	 *
-	 * @param float $x    Original value.
-	 * @param float $oMin Old minimum.
-	 * @param float $oMax Old maximum.
-	 * @param float $nMin New minimum.
-	 * @param float $nMax New maximum.
-	 *
-	 * @return bool|float Return false on failure, or return new value within new range.
-	 */
-	public static function remapRange( $x, $oMin, $oMax, $nMin, $nMax ) {
-
-		_deprecated_function( __METHOD__, '9.11', '_::remapRange()' );
-
-		return _::remapRange( $x, $oMin, $oMax, $nMin, $nMax );
 	}
 }

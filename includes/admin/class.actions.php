ff145a1ffbec636d465b6cc0e52c42d5046fc8f7
Steven A. Zahm <shazahm1@hotmail.com>
2022-10-07 15:43:10 -0400

    Utilize `\Request\Admin_Action` to get request action.

--- includes/admin/class.actions.php
+++ includes/admin/class.actions.php
@@ -1305,7 +1305,7 @@ class cnAdminActions {
 	public static function processEntry() {
 
 		$form   = new cnFormObjects();
-		$action = isset( $_REQUEST['cn-action'] ) ? sanitize_key( $_REQUEST['cn-action'] ) : '';
+		$action = Request\Admin_Action::from( INPUT_POST )->value();
 
 		// Set up the redirect URL.
 		$redirect = isset( $_POST['redirect'] ) ? wp_sanitize_redirect( $_POST['redirect'] ) : 'admin.php?page=connections_add';

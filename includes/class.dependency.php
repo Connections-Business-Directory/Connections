d370e9715816eb831af231a29bb3d5dad5dc4214
Steven A. Zahm <shazahm1@hotmail.com>
2022-11-10 13:04:22 -0500

    Introduce `\Hook\Action\Admin\Log_Management`.

--- includes/class.dependency.php
+++ includes/class.dependency.php
@@ -582,6 +582,7 @@ class cnDependency {
 
 			// Hooks.
 			'Connections_Directory\Hook\Action'                              => 'includes/Hook/Action.php',
+			'Connections_Directory\Hook\Action\Admin\Log_Management'         => 'includes/Hook/Action/Admin/Log_Management.php',
 			'Connections_Directory\Hook\Action\Admin\Role_Capability'        => 'includes/Hook/Action/Admin/Role_Capability.php',
 			'Connections_Directory\Hook\Action\Admin\Template'               => 'includes/Hook/Action/Admin/Template.php',
 			'Connections_Directory\Hook\Action\Ajax\Category_Metabox_Height' => 'includes/Hook/Action/Ajax/Category_Metabox_Height.php',

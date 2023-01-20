5aebc62de3d4c45f6c953e0fd9614f3d59df5daf
Steven A. Zahm <shazahm1@hotmail.com>
2023-01-20 13:22:07 -0500

    Update copyright year in file header.

--- includes/shortcode/inc.shortcodes.php
+++ includes/shortcode/inc.shortcodes.php
@@ -4,7 +4,7 @@
  *
  * @package     Connections
  * @subpackage  Shortcodes
- * @copyright   Copyright (c) 2013, Steven A. Zahm
+ * @copyright   Copyright (c) 2023, Steven A. Zahm
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       unknown
  */

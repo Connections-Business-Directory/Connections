d28d2c7470f8672736dc915cc0ee88f8dd9bdcc9
Steven A. Zahm <shazahm1@hotmail.com>
2023-02-24 16:11:06 -0500

    Introduce the `Search` and `Submit` fields to the Fields API.

---
+++ includes/Form/Field/Search.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Connections_Directory\Form\Field;
+
+/**
+ * Class Search
+ *
+ * @package Connections_Directory\Form\Field
+ */
+class Search extends Text {
+
+	/**
+	 * The Input field type.
+	 *
+	 * @since 10.4.39
+	 * @var string
+	 */
+	protected $type = 'search';
+
+}
--- includes/class.dependency.php
+++ includes/class.dependency.php
@@ -383,8 +383,10 @@ class cnDependency {
 			'Connections_Directory\Form\Field\Radio'                  => 'includes/Form/Field/Radio.php',
 			'Connections_Directory\Form\Field\Radio_Group'            => 'includes/Form/Field/Radio_Group.php',
 			'Connections_Directory\Form\Field\Rich_Text'              => 'includes/Form/Field/Rich_Text.php',
+			'Connections_Directory\Form\Field\Search'                 => 'includes/Form/Field/Search.php',
 			'Connections_Directory\Form\Field\Select'                 => 'includes/Form/Field/Select.php',
 			'Connections_Directory\Form\Field\Slider'                 => 'includes/Form/Field/Slider.php',
+			'Connections_Directory\Form\Field\Submit'                 => 'includes/Form/Field/Submit.php',
 			'Connections_Directory\Form\Field\Text'                   => 'includes/Form/Field/Text.php',
 			'Connections_Directory\Form\Field\Textarea'               => 'includes/Form/Field/Textarea.php',
 

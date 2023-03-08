f04eb9594abad385434cf7ccf16c5aa51539e068
Steven A. Zahm <shazahm1@hotmail.com>
2023-03-08 16:06:58 -0500

    Inline comments must end in full-stops, exclamation marks, or question marks.

--- includes/template/class.template-shortcode.php
+++ includes/template/class.template-shortcode.php
@@ -223,31 +223,31 @@ class cnTemplate_Shortcode {
 		// WARNING! Do not change this regex without changing do_shortcode_tag() and strip_shortcode_tag()
 		// Also, see shortcode_unautop() and shortcode.js.
 		return
-			  '\\['                              // Opening bracket
+			  '\\['                              // Opening bracket.
 			. '(\\[?)'                           // 1: Optional second opening bracket for escaping shortcodes: [[tag]]
 			. "($tagregexp)"                     // 2: Shortcode name
-			. '(?![\\w-])'                       // Not followed by word character or hyphen
+			. '(?![\\w-])'                       // Not followed by word character or hyphen.
 			. '('                                // 3: Unroll the loop: Inside the opening shortcode tag
-			.     '[^\\]\\/]*'                   // Not a closing bracket or forward slash
+			.     '[^\\]\\/]*'                   // Not a closing bracket or forward slash.
 			.     '(?:'
-			.         '\\/(?!\\])'               // A forward slash not followed by a closing bracket
-			.         '[^\\]\\/]*'               // Not a closing bracket or forward slash
+			.         '\\/(?!\\])'               // A forward slash not followed by a closing bracket.
+			.         '[^\\]\\/]*'               // Not a closing bracket or forward slash.
 			.     ')*?'
 			. ')'
 			. '(?:'
 			.     '(\\/)'                        // 4: Self closing tag ...
-			.     '\\]'                          // ... and closing bracket
+			.     '\\]'                          // ... and closing bracket.
 			. '|'
-			.     '\\]'                          // Closing bracket
+			.     '\\]'                          // Closing bracket.
 			.     '(?:'
-			.         '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags
-			.             '[^\\[]*+'             // Not an opening bracket
+			.         '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags.
+			.             '[^\\[]*+'             // Not an opening bracket.
 			.             '(?:'
-			.                 '\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag
-			.                 '[^\\[]*+'         // Not an opening bracket
+			.                 '\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag.
+			.                 '[^\\[]*+'         // Not an opening bracket.
 			.             ')*+'
 			.         ')'
-			.         '\\[\\/\\2\\]'             // Closing shortcode tag
+			.         '\\[\\/\\2\\]'             // Closing shortcode tag.
 			.     ')?'
 			. ')'
 			. '(\\]?)';                          // 6: Optional second closing brocket for escaping shortcodes: [[tag]]

e01ea221c112ffa952c41d4b5eeb25ba5b6a60d0
Steven A. Zahm <shazahm1@hotmail.com>
2023-04-04 14:25:01 -0400

    Remove the deprecated `cnGeo::BG_Regions()` method.

--- includes/geo/class.geo.php
+++ includes/geo/class.geo.php
@@ -409,25 +409,6 @@ class cnGeo {
 	}
 
 	/**
-	 * Get the Bulgarian regions.
-	 *
-	 * @access public
-	 * @since  8.1.6
-	 * @static
-	 * @deprecated 8.7
-	 *
-	 * @return array An associative array of regions where the key is the region abbr and the value is the full region name.
-	 */
-	public static function BG_Regions() {
-
-		$regions = cnCountries::getByCode( 'bg' )->getDivisions();
-		$regions = wp_list_pluck( $regions, 'name' );
-		natsort( $regions );
-
-		return apply_filters( 'cn_bulgarian_regions', $regions );
-	}
-
-	/**
 	 * Get the Canadian regions.
 	 *
 	 * @access public

87038fcdb7e5f56ed6202c50a5f3e3cfbf708a65
Steven A. Zahm <shazahm1@hotmail.com>
2023-04-04 14:26:21 -0400

    Remove the deprecated `cnGeo::AU_Regions()` method.

--- includes/geo/class.geo.php
+++ includes/geo/class.geo.php
@@ -409,25 +409,6 @@ class cnGeo {
 	}
 
 	/**
-	 * Get the Australian regions.
-	 *
-	 * @access public
-	 * @since  0.8
-	 * @static
-	 * @deprecated 8.7
-	 *
-	 * @return array An associative array of regions where the key is the region abbr and the value is the full region name.
-	 */
-	public static function AU_Regions() {
-
-		$regions = cnCountries::getByCode( 'au' )->getDivisions();
-		$regions = wp_list_pluck( $regions, 'name' );
-		natsort( $regions );
-
-		return apply_filters( 'cn_au_regions', $regions );
-	}
-
-	/**
 	 * Get the Bangladeshi regions (Districts).
 	 *
 	 * @access public

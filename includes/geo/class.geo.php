292ce6b75607edb6613a514ce07b424051bb64fe
Steven A. Zahm <shazahm1@hotmail.com>
2023-04-04 14:26:56 -0400

    Remove the deprecated `cnGeo::BD_Regions()` method.

--- includes/geo/class.geo.php
+++ includes/geo/class.geo.php
@@ -409,25 +409,6 @@ class cnGeo {
 	}
 
 	/**
-	 * Get the Bangladeshi regions (Districts).
-	 *
-	 * @access public
-	 * @since  8.1.6
-	 * @static
-	 * @deprecated 8.7
-	 *
-	 * @return array An associative array of regions where the key is the region abbr and the value is the full region name.
-	 */
-	public static function BD_Regions() {
-
-		$regions = cnCountries::getByCode( 'bd' )->getDivisions();
-		$regions = wp_list_pluck( $regions, 'name' );
-		natsort( $regions );
-
-		return apply_filters( 'cn_bangladeshi_regions', $regions );
-	}
-
-	/**
 	 * Get the Brazil regions.
 	 *
 	 * @access public

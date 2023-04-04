4263c8d4c06f00a21f925976eebf7ca0a6cd0a81
Steven A. Zahm <shazahm1@hotmail.com>
2023-04-04 14:30:25 -0400

    Remove the deprecated `cnGeo::HU_Regions()` method.

--- includes/geo/class.geo.php
+++ includes/geo/class.geo.php
@@ -409,25 +409,6 @@ class cnGeo {
 	}
 
 	/**
-	 * Get the Hungary regions.
-	 *
-	 * @access public
-	 * @since  0.8
-	 * @static
-	 * @deprecated 8.7
-	 *
-	 * @return array An associative array of regions where the key is the region abbr and the value is the full region name.
-	 */
-	public static function HU_Regions() {
-
-		$regions = cnCountries::getByCode( 'hu' )->getDivisions();
-		$regions = wp_list_pluck( $regions, 'name' );
-		natsort( $regions );
-
-		return apply_filters( 'cn_hu_regions', $regions );
-	}
-
-	/**
 	 * Get the Chinese regions.
 	 *
 	 * @access public

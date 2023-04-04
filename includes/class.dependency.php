51ba1e6a583ff2c51bafcc398404e9abd3ae9e42
Steven A. Zahm <shazahm1@hotmail.com>
2023-04-04 14:55:43 -0400

    Introduce the `Do_Shortcode` trait.

--- includes/class.dependency.php
+++ includes/class.dependency.php
@@ -404,6 +404,7 @@ class cnDependency {
 			'Connections_Directory\Shortcode\mapBlock'            => 'includes/shortcode/class.shortcode-mapblock.php',
 			'Connections_Directory\Shortcode\Entry'               => 'includes/shortcode/Entry.php',
 			'Connections_Directory\Shortcode\Conditional_Content' => 'includes/shortcode/Conditional_Content.php',
+			'Connections_Directory\Shortcode\Do_Shortcode'        => 'includes/shortcode/Do_Shortcode.php',
 			'cnThumb'                                             => 'includes/shortcode/class.shortcode-thumbnail.php',
 			'cnThumb_Responsive'                                  => 'includes/shortcode/class.shortcode-thumbnail-responsive.php',
 

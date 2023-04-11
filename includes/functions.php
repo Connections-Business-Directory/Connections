714b4b365d0cf9f08382f56c7222e67ea72b4d01
Steven A. Zahm <shazahm1@hotmail.com>
2023-04-11 16:45:29 -0400

    Move the `connectionsEntryList()` and `connectionsUpcomingList()` template tags to the global functions.php file.

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+use Connections_Directory\Shortcode\Upcoming_List;
 use Connections_Directory\Taxonomy;
 use Connections_Directory\Taxonomy\Registry;
 use function Connections_Directory\Sitemaps\createProvider;
@@ -45,4 +46,35 @@ function cn_register_taxonomy( $taxonomy, $args = array() ) {
 	$taxonomies = Registry::get();
 
 	return $taxonomies->register( $taxonomy, $args );
+}
+
+/**
+ * Template tag to call the entry list. All options can be passed as an
+ * associative array. The options are identical to those available to the
+ * shortcode.
+ *
+ * EXAMPLE:   connectionsEntryList( array('id' => 325) );
+ *
+ * @access public
+ * @since  unknown
+ *
+ * @param array $atts
+ */
+function connectionsEntryList( $atts ) {
+	// HTML is escaped within shortcode callback and the template files.
+	echo cnShortcode_Connections::shortcode( $atts ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+}
+
+/**
+ * Template tag to call the upcoming list. All options can be passed as an
+ * associative array. The options are identical to those available to the
+ * shortcode.
+ *
+ * EXAMPLE:   connectionsUpcomingList(array('days' => 30));
+ *
+ * @param array $atts
+ */
+function connectionsUpcomingList( $atts ) {
+
+	Upcoming_List::instance( $atts )->render();
 }

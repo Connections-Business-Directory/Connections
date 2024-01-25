890bf6b61ed4f1e250c82f771c0ec378ede3aa41
Steven A. Zahm <shazahm1@hotmail.com>
2024-01-25 16:46:58 -0500

    Introduce the Settings Reset tool.

--- includes/class.dependency.php
+++ includes/class.dependency.php
@@ -608,6 +608,7 @@ class cnDependency {
 			'Connections_Directory\Hook\Action\Ajax\Database_Reset'             => 'includes/Hook/Action/Ajax/Database_Reset.php',
 			'Connections_Directory\Hook\Action\Ajax\Response'                   => 'includes/Hook/Action/Ajax/Response.php',
 			'Connections_Directory\Hook\Action\Ajax\Settings_Export_Import'     => 'includes/Hook/Action/Ajax/Settings_Export_Import.php',
+			'Connections_Directory\Hook\Action\Ajax\Settings_Reset'             => 'includes/Hook/Action/Ajax/Settings_Reset.php',
 			'Connections_Directory\Hook\Action\Ajax\System_Information'         => 'includes/Hook/Action/Ajax/System_Information.php',
 			'Connections_Directory\Hook\Filter\Admin\Footer'                    => 'includes/Hook/Filter/Admin/Footer.php',
 			'Connections_Directory\Hook\Filter\Admin\Plugin_Row'                => 'includes/Hook/Filter/Admin/Plugin_Row.php',
